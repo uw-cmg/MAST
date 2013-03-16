8d71b46a79e5025d03ed6d3ca9a9afe2a525ba4e
Tam Mayeshiba <mayeshiba@wisc.edu>
2013-03-15 23:57:51 -0500

    add error statement if can't get structure

--- MAST/MAST/ingredients/checker/vasp_checker.py
+++ MAST/MAST/ingredients/checker/vasp_checker.py
@@ -5,6 +5,11 @@ import os
 def get_structure_from_parent(parentpath):
     """Get structure from VASP output."""
     structpath = os.path.join(parentpath, "CONTCAR")
-    parent_structure = Poscar.from_file(structpath, False).structure
+    try:
+        parent_structure = Poscar.from_file(structpath, False).structure
+    except:
+        print "Error getting structure from CONCAR."
+        print "Make except statement more specific."
+        return None
     return parent_structure
 
