07c63f7489bc0727255164d730be8855326975eb
MLman <kimbellw@gmail.com>
2013-03-29 01:47:40 -0500

    File manager class and example file

--- DAG/picklemanager.py
+++ DAG/picklemanager.py
@@ -8,12 +8,23 @@ class PickleManager:
         else:
             self.fn = filename
 
-    def save_variables(self, varlist, gdict, filename=None):
-        '''gdict = globals()'''
+    def save_variables(self, gdict, varlist=None, filename=None):
+        '''gdict = globals() or gdict = vardict
+            Usage1:
+                >>> pm.save_variables(gdict=globals(), varlist=['What','You','Want']) # default filename is self.fn
+                >>> pm.save_variables(gdict=globals(), varlist=['What','You','Want'], filename='yourpickle.pkl')
+            Usage2:
+                >>> pm.save_variables(gdict=yourvardict)
+                >>> pm.save_variables(gdict=yourvardict, filename='yourpickle.pkl')
+        '''
         if filename is None:
             fh = open(self.fn,'wb')
         else:
-            fh = open(filename=None,'wb')
+            fh = open(filename,'wb')
+
+        if varlist is None:
+            varlist = list(gdict)
+            print varlist
 
         pickle.dump(varlist,fh)
         for key in varlist:
@@ -31,13 +42,13 @@ class PickleManager:
         varlist = pickle.load(fh)
         for key in varlist:
             vardict[key] = pickle.load(fh)
-        
+        fh.close()
         return vardict
 
-    def load_variables_to_ws(self, filename=None, gdict):
+    def load_variables_to_ws(self, gdict, filename=None):
         ''' Load variables to workspace from a pickle file.
-        Usage 1 : load_variables_to_ws(self, gdict = globals())
-        Usage 2 : load_variables_to_ws(self, filename = 'savefile.pkl',gdict = globals())
+        Usage 1 : load_variables_to_ws(self, gdict=globals())
+        Usage 2 : load_variables_to_ws(self, filename='savefile.pkl', gdict=globals())
         '''
         if filename is None:
             fn = self.fn
@@ -48,7 +59,9 @@ class PickleManager:
         for key in varlist:
             if key in gdict:
                 print '%s is overwritten.' % key
+            else:
+                print '%s is loaded.' % key
             gdict[key] = pickle.load(fh)
-
+        fh.close()
 
 
