7c7d52da69a0baf95d37c28954cd93e8d612db4e
MLman <kimbellw@gmail.com>
2013-03-30 13:55:22 -0500

    Revert "Revert "Merge branch 'dev' of https://github.com/cmgtam/MAST4pymatgen into dev""
    This reverts commit d1028729d195e89ef6db3ccaab670e418a58b8a1.

--- MAST/ingredients/optimize.py
+++ MAST/ingredients/optimize.py
@@ -1,13 +1,16 @@
 import numpy as np
 
 from pymatgen.core.structure import Structure
-#from pymatgen.core.structure_modifier import StructureEditor
-#from pymatgen.util.coord_utils import find_in_coord_list
 from pymatgen.io.vaspio import Poscar
 from pymatgen.io.vaspio import Outcar
+from pymatgen.io.vaspio import Kpoints
+from pymatgen.io.vaspio import Potcar
+from pymatgen.io.vaspio import Incar
 
+from MAST.ingredients.pmgextend import vasp_extensions
 from MAST.utility import MASTObj
 from MAST.ingredients import BaseIngredient
+from MAST.utility import MASTError
 
 import os
 import shutil
@@ -17,63 +20,29 @@ import shutil
 class Optimize(BaseIngredient):
     def __init__(self, **kwargs):
         allowed_keys = {
-                'dir_name' : (str, str(), 'Name of optimization directory'),
-                'ibrion': (str, str(), 'IBRION for optimization (default 2)'),
-                'structure_init': (Structure, None, 'Pymatgen Structure object'),
-                #'structure_final': (Structure, None, 'Pymatgen Structure object'),#TA never need for opt, right?
-                'parent_init': (str, str(), 'Directory of initial state'),
-                #'parent_final': (str, str(), 'Directory of final state'), #TA also don't need
-                'program': (str, str(), 'DFT program, e.g. "vasp"')
-                }
+            'name' : (str, str(), 'Name of optimization directory'),
+            'program': (str, str(), 'DFT program, e.g. "vasp"'),
+            'program_keys': (dict, dict(), 'Dictionary of program keywords'),
+            'child_dict': (dict, dict(), 'Dictionary of children'),
+            'structure': (Structure, None, 'Pymatgen Structure object')
+            }
         BaseIngredient.__init__(self, allowed_keys, **kwargs)
 
     def is_complete(self):
-        return BaseIngredient.is_complete() #instead call base ingredient complete check
-        #if self.keywords['program'] == "vasp":
-        #    try:
-        #        myoutcar = Outcar(os.path.join(dir_name, "OUTCAR"))
-        #    except (IOError):
-        #        return False
-                
+        return BaseIngredient.is_complete(self) #instead call base ingredient complete check
 
-        #    if not myoutcar.run_stats['User time (sec)'] > 0:
-        #        return False
-        #    else:
-        #        return True
-        #else:
-        #    print "Program " + self.keywords['program'] + " not yet supported for optimize."
-        #    return None
-    
-    def get_structure_from_parent(self):
-        return BaseIngredient.get_structure_from_parent(self.keywords['parent_init'])
-        #if self.keywords['program'] == "vasp":
-        #    structpath = os.path.join(parentpath, "CONTCAR")
-        #    parent_structure = Poscar.from_file(structpath, False).structure
-        #    return parent_structure
-        #else:
-        #    print "Could not get structure."
-        #    return None
-
-    def get_energy_file_from_parent(self, parentpath, program=""): #TA I think program param should default to using keywrd
-        if program == "":
-            program = self.keywords['program']
-        if program == "vasp":
-            return os.path.join(parentpath, "OSZICAR")
+    def update_children(self):
+        for childname in self.keywords['child_dict'].iterkeys():
+            self.forward_parent_structure(self.keywords['name'], childname)       
+    def write_files(self):
+        if self.keywords['program'] == 'vasp':
+            self.set_up_vasp_optimize()
         else:
-            print "Could not get energy file."
-            return None
-
-    def generate_files(self):
-        if not self.keywords['parent_init'] == None: #TA could be empty for optimize
-            struct_init = self.get_structure_from_parent()
-        else: #TA if empty then use the keyword structure
-            struct_init = self.keywords['structure_init']
-        self.set_up_vasp_optimize(struct_init)
-        return
+            raise MASTError(self.__class__.__name__, "Program not supported.")
 
     def set_up_vasp_incar_dict(self, rep_structure, rep_potcar):
         myd=dict()
-        myd['IBRION']=self.keywords['ibrion']
+        myd['IBRION']=self.keywords['program_keys']['ibrion']
         myd['ISIF']=3
         myd['NSW']=191
         myd['NPAR']=4 #hardcoded - needs fixing
@@ -85,26 +54,25 @@ class Optimize(BaseIngredient):
         myd['LWAVE']="False"
         myd['NSW']=191
         myd['MAGMOM']=5*len(rep_structure.sites)
-        myd['ENCUT']=MAST.ingredients.pmgextend.vasp_extend.get_max_enmax_from_potcar(rep_potcar)*1.5
+        myd['ENCUT']=vasp_extensions.get_max_enmax_from_potcar(rep_potcar)*1.5
         return myd
 
     
-    def set_up_vasp_optimize(self, struct_init):
-        dir_name = self.keywords['dir_name']
-        if os.path.exists(dir_name):
-            print "Directory exists."
-            return
-        os.makedir(dir_name)
-        topkpoints = pymatgen.io.vaspio.Kpoints.monkhorst_automatic(kpts=(4,4,4),shift=(0,0,0))
-        topkpoints.write_file(dirname + "/KPOINTS")
-        toppotcar = pymatgen.io.vaspio.Potcar(symbols=Poscar(struct_init).site_symbols, functional='PAW-GGA', sym_potcar_map=None)
-        toppotcar.write_file(dirname + "/POTCAR")
-        topincar = pymatgen.io.vaspio.Incar()
-        incar_dict = self.set_up_vasp_incar_dict(struct_init, toppotcar)
-        topincar.from_dict(incar_dict)
-        topincar.write_file(dirname + "/INCAR")
-        opt_poscar = Poscar(struct_init)
-        pospath = os.path.join(dir_name, "POSCAR")
-        opt_poscar.write_file(pospath)
+    def set_up_vasp_optimize(self):
+        name = self.keywords['name']
+        pospath = os.path.join(name, "POSCAR")
+        if self.keywords['structure'] == None:
+            opt_poscar = Poscar.from_file(pospath) 
+            #parent should have given a structure
+        else: #this is an originating run; mast should give it a structure
+            opt_poscar = Poscar(self.keywords['structure'])
+            opt_poscar.write_file(pospath)
+        topkpoints = Kpoints.monkhorst_automatic(kpts=(4,4,4),shift=(0,0,0))
+        topkpoints.write_file(name + "/KPOINTS")
+        toppotcar = Potcar(symbols=opt_poscar.site_symbols, functional='PBE', sym_potcar_map=None)
+        toppotcar.write_file(name + "/POTCAR")
+        incar_dict = self.set_up_vasp_incar_dict(opt_poscar.structure, toppotcar)
+        topincar = Incar(incar_dict)
+        topincar.write_file(name + "/INCAR")
         return
 
--- MAST/ingredients/performneb.py
+++ MAST/ingredients/performneb.py
@@ -1,13 +1,15 @@
-import numpy as np
 
-import pymatgen
 from pymatgen.core.structure import Structure
 from pymatgen.io.vaspio import Poscar
 from pymatgen.io.vaspio import Outcar
+from pymatgen.io.vaspio import Kpoints
+from pymatgen.io.vaspio import Potcar
+from pymatgen.io.vaspio import Incar
 
 from MAST.ingredients.pmgextend import vasp_extensions
 from MAST.utility import MASTObj
 from MAST.ingredients import BaseIngredient
+from MAST.utility import MASTError
 
 import os
 import shutil
@@ -18,40 +20,81 @@ class PerformNEB(BaseIngredient):
     def __init__(self, **kwargs):
         #pdb.set_trace()
         allowed_keys = {
-                'dir_name' : (str, str(), 'Name of NEB directory'),
-                'parent_init': (str, str(), 'Directory of initial state'),
-                'parent_final': (str, str(), 'Directory of final state'),
-                'images': (int, 0, 'Number of images'),
-                'program': (str, str(), 'DFT program, e.g. "vasp"')
+                'name' : (str, str(), 'Name of NEB directory'),
+                'program': (str, str(), 'DFT program, e.g. "vasp"'),
+                'program_keys': (dict, dict(), 'Program-specific keywords'),
+                'child_dict':(dict, dict(), 'Dictionary of child names'),
+                'structure': (Structure, None, 'Pymatgen-type structure')
                 }
         BaseIngredient.__init__(self, allowed_keys, **kwargs)
-        #prog_kwarg_dict = options.get_item('vasp',ingredient_name)
 
     def is_complete(self):
         return BaseIngredient.images_complete(self)
 
-    def generate_files(self):
-        image_structures = self.do_interpolation()
+    def update_children(self):
+        myct=1
+        impath=""
+        for childname in sorted(self.keywords['child_dict'].iterkeys()):
+            impath = os.path.join(self.keywords['name'], str(myct).zfill(2))
+            self.forward_parent_structure(impath, childname)       
+            myct = myct + 1
+
+    def write_files(self):
+        parentstructures = self.get_parent_structures()
+        image_structures = self.do_interpolation(parentstructures)
+        if image_structures == None:
+            raise MASTError(self.__class__.__name__,"Bad number of images")
         if self.keywords['program'] == 'vasp':
             self.set_up_vasp_neb(image_structures)
         else:
-            print "Program not supported. No setup accomplished."
-            return
+            raise MASTError(self.__class__.__name__,"Program not supported. No setup accomplished.")
         return
    
-    def do_interpolation(self):
-        struct_init = None
-        struct_fin = None
-        struct_init = BaseIngredient.get_structure_from_parent(self, self.keywords['parent_init'])
-        struct_fin = BaseIngredient.get_structure_from_parent(self, self.keywords['parent_final'])
-        if (struct_init == None) or (struct_fin == None):
-            print "Error getting initial or final parent structure."
-            return
-        structure_list = struct_init.interpolate(struct_fin, self.keywords['images'] + 1 )
+    def get_my_numbers(self):
+        """For neb in the format <sys>_neb<N>-<N> return the two
+            key numbers identifying the defect sites.
+        """
+        tempname = self.keywords['name'].lower()
+        numstr = tempname[tempname.find("neb")+3:]
+        if numstr.find("neb") == -1:
+            pass
+        else:
+            numstr = numstr[numstr.find("neb")+3:] #allow 'neb' to be in <sys>
+        numparents = numstr.split('-')
+        if not(len(numparents) == 2):
+            raise MASTError(self.__class__.__name__,"Error: number of parents is not two for NEB in " + self.keywords['name'])
+            return None
+        return numparents
+
+
+    def get_parent_structures(self):
+        """Assume that parents have written two files,
+            named 'parent_structure_<N>'. 
+            For VASP these are CONTCAR-type files.
+            Returns:
+                [struct_init, struct_fin]: pymatgen Structure objects
+        """
+        numparents = self.get_my_numbers()
+        pfpath1=os.path.join(self.keywords['name'], "parent_structure_" + numparents[0])
+        if not os.path.isfile(pfpath1):
+            raise MASTError(self.__class__.__name__,"Error: no parent file at" + pfpath1)
+        struct_init = BaseIngredient.get_structure_from_file(self, pfpath1)
+        pfpath2=os.path.join(self.keywords['name'], "parent_structure_" + numparents[1])
+        if not os.path.isfile(pfpath2):
+            raise MASTError(self.__class__.__name__,"Error: no parent file at" + pfpath2)
+        struct_fin = BaseIngredient.get_structure_from_file(self, pfpath2)
+        return [struct_init, struct_fin]
+
+    def do_interpolation(self, parentstructures):
+        """Do interpolation."""
+        if parentstructures == None:
+            raise MASTError(self.__class__.__name__,"Bad number of parent paths.")
+        struct_init = parentstructures[0]
+        struct_fin = parentstructures[1]
+        structure_list = struct_init.interpolate(struct_fin, self.keywords['program_keys']['images'] + 1 )
         return structure_list
 
     def set_up_vasp_incar_dict(self, rep_structure, rep_potcar):
-
         myd=dict()
         myd['IBRION']=1
         myd['POTIM']=0.5
@@ -63,46 +106,60 @@ class PerformNEB(BaseIngredient):
         myd['ISMEAR']=0
         myd['SIGMA']=0.05
         myd['ISPIN']=2
-        myd['IMAGES']=self.keywords['images']
+        myd['IMAGES']=self.keywords['program_keys']['images']
         myd['LCHARG']="False"
         myd['LWAVE']="False"
         myd['NSW']=191
         myd['MAGMOM']=str(len(rep_structure.sites)) + "*5"
         myd['ENCUT']=vasp_extensions.get_max_enmax_from_potcar(rep_potcar)*1.5
         return myd
+    
+
+    def place_parent_energy_files_vasp(self):
+        """Assume parents have written files parent_energy_<N>.
+            Copy these files into the 00 and 0N directories.
+        """
+        numparents = self.get_my_numbers()
+        if numparents == None:
+            return
+        pfpath1=os.path.join(self.keywords['name'], "parent_energy_" + numparents[0])
+        if not os.path.isfile(pfpath1):
+            raise MASTError(self.__class__.__name__,"Error: no parent file at" + pfpath1)
+        shutil.copy(pfpath1, os.path.join(self.keywords['name'],"00","OSZICAR"))
+        pfpath2=os.path.join(self.keywords['name'], "parent_energy_" + numparents[1])
+        if not os.path.isfile(pfpath2):
+            raise MASTError(self.__class__.__name__,"Error: no parent file at" + pfpath2)
+        shutil.copy(pfpath2, os.path.join(self.keywords['name'],str(self.keywords['program_keys']['images']+1).zfill(2),"OSZICAR"))
+        return
 
     def set_up_vasp_folders(self, image_structures):
-        dir_name = self.keywords['dir_name']
-        if os.path.exists(dir_name):
-            print "Directory exists."
-            return
-        os.makedirs(dir_name)
         imct=0
-        while imct <= self.keywords['images'] + 1:
+        while imct <= self.keywords['program_keys']['images'] + 1:
             imposcar = Poscar(image_structures[imct])
             num_str = str(imct).zfill(2)
-            impath = os.path.join(dir_name, num_str)
-            impospath = os.path.join(dir_name, "POSCAR_" + num_str)
+            impath = os.path.join(self.keywords['name'], num_str)
+            impospath = os.path.join(self.keywords['name'], "POSCAR_" + num_str)
             imposcar.write_file(impospath)
-            os.makedirs(impath)
+            try:
+                os.makedirs(impath)
+            except OSError:
+                print "Directory at", impath, "already exists."
+                return None
             imposcar.write_file(os.path.join(impath, "POSCAR"))
-            if imct == 0:
-                shutil.copy(os.path.join(self.keywords['parent_init'],"OSZICAR"),impath)
-            elif imct == self.keywords['images'] + 1:
-                shutil.copy(os.path.join(self.keywords['parent_final'],"OSZICAR"),impath)
             imct = imct + 1
+        self.place_parent_energy_files_vasp()
         return
         
 
     def set_up_vasp_neb(self, image_structures):
         self.set_up_vasp_folders(image_structures)
-        dir_name = self.keywords['dir_name']
-        topkpoints = pymatgen.io.vaspio.Kpoints.monkhorst_automatic(kpts=(4,4,4),shift=(0,0,0))
+        dir_name = self.keywords['name']
+        topkpoints = Kpoints.monkhorst_automatic(kpts=(4,4,4),shift=(0,0,0))
         topkpoints.write_file(dir_name + "/KPOINTS")
-        toppotcar = pymatgen.io.vaspio.Potcar(symbols=Poscar(image_structures[0]).site_symbols, functional='PBE', sym_potcar_map=None)
+        toppotcar = Potcar(symbols=Poscar(image_structures[0]).site_symbols, functional='PBE', sym_potcar_map=None)
         toppotcar.write_file(dir_name + "/POTCAR")
         incar_dict = self.set_up_vasp_incar_dict(image_structures[0], toppotcar)
-        topincar = pymatgen.io.vaspio.Incar(incar_dict)
+        topincar = Incar(incar_dict)
         topincar.write_file(dir_name + "/INCAR")
         return
 
