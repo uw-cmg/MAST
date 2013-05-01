f831051ef7f12ab9e07b05e666c0649c87c9c98c
Tam Mayeshiba <mayeshiba@wisc.edu>
2013-05-01 16:03:03 -0500

    Use ingredients' mast_defects dictionary for defects

--- MAST/ingredients/inducedefect.py
+++ MAST/ingredients/inducedefect.py
@@ -30,36 +30,43 @@ class InduceDefect(BaseIngredient):
 
     def induce_defect(self):
         """Creates a defect, and returns the modified structure
+            mast_defect is a dictionary like this: 
+            'defect1': {'symbol': 'cr', 'type': 'interstitial', 
+                        'coordinates': array([ 0. ,  0.5,  0. ])}}
+            'defect2': {'symbol': 'o', 'type': 'vacancy', 
+                        'coordinates': array([ 0.25,  0.25,  0.25])}
+            'coord_type': 'fractional' 
         """
-        print self.keywords
-        struct_ed = StructureEditor(self.keywords['structure'])
+        myidx = "defect" + str(self.get_my_number())
+        mydict = self.keywords['program_keys']['mast_defects'][myidx]
+        struct_ed = StructureEditor(self.keywords['structure']) #should be updated using get_new_structure)
 
-        if (self.keywords['coordtype'] == 'cartesian'):
-            self.keywords['position'] = self._cart2frac(self.keywords['position'])
+        if (self.keywords['program_keys']['mast_defects']['coord_type'] == 'cartesian'):
+            mydict['coordinates'] = self._cart2frac(mydict['coordinates'])
 
-        if (self.keywords['defecttype'].lower() == 'vacancy'):
+        if (mydict['type'].lower() == 'vacancy'):
             print 'Making a vacancy!'
 
             index = find_in_coord_list(self.keywords['structure'].frac_coords,
-                                       self.keywords['position'],
+                                       mydict['coordinates'],
                                        atol=1e-04)
 
             struct_ed.delete_site(index)
-        elif (self.keywords['defecttype'].lower() == 'interstitial'):
+        elif (mydict['type'].lower() == 'interstitial'):
             print 'Making an interstitial!'
 
-            struct_ed.append_site(self.keywords['atom'],
-                                  self.keywords['position'],
+            struct_ed.append_site(mydict['symbol'],
+                                  mydict['coordinates'],
                                   coords_are_cartesian=False,
                                   validate_proximity=False) # Should be set to True!
-        elif (self.keywords['defecttype'].lower() == 'antisite'):
+        elif (mydict['type'].lower() == 'antisite'):
             print 'Making an antisite!'
 
             index = find_in_coord_list(self.keywords['structure'].frac_coords,
-                                       self.keywords['position'],
+                                       mydict['coordinates'],
                                        atol=1e-04)
 
-            struct_ed.replace_site(index, self.keywords['atom'])
+            struct_ed.replace_site(index, mydict['symbol'])
         else:
             raise RuntimeError('Defect type %s not supported' % self.keywords['defecttype'])
 
@@ -102,4 +109,16 @@ class InduceDefect(BaseIngredient):
         else:
             raise MASTError(self.__class__.__name__, "Program not supported.")
         return
-        
+    
+    def get_my_number(self):
+        """For defect in the format <sys>_induce_defect<N>, return N.
+        """
+        tempname = self.keywords['name'].lower()
+        numstr = tempname[tempname.find("defect")+6:]
+        if numstr.find("defect") == -1:
+            pass
+        else:
+            numstr = numstr[numstr.find("defect")+6:] #allow 'defect' to be in <sys>
+        defectstr = numstr
+        return defectstr
+
