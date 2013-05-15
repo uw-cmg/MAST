fec66ac66b9ab24614faa7b1fc23c5ce6a8bd43b
Tam Mayeshiba <mayeshiba@wisc.edu>
2013-05-15 08:36:09 -0500

    remove extra imports from inherited ingredients

--- MAST/ingredients/static.py
+++ MAST/ingredients/static.py
@@ -1,6 +1,4 @@
 from MAST.ingredients.optimize import Optimize
-from MAST.ingredients.baseingredient import BaseIngredient
-from pymatgen.core.structure import Structure
 
 class Static(Optimize):
     def __init__(self, **kwargs):
