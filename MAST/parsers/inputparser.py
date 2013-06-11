644de4e89c012330f2b001ac4e48a1aff404d15e
Tam Mayeshiba <mayeshiba@wisc.edu>
2013-06-11 16:28:00 -0500

    remove pdb

--- MAST/parsers/inputparser.py
+++ MAST/parsers/inputparser.py
@@ -206,8 +206,6 @@ class InputParser(MASTObj):
                     elname = elline[1].strip().title() #Title case
                     element_map[elkey]=elname
 
-        import pdb
-        pdb.set_trace() 
         if len(element_map) > 0 and len(atom_list) > 0:
             new_atom_list=list()
             for atomval in atom_list:
