3a149b329afca44fad087fbd6f5b4b051b3b0aa3
Glen Jenness <gjenness@gmail.com>
2013-06-26 16:46:24 -0500

    Issue #142 Adding final touches onto the DFE tool

--- MAST/utility/defect_formation_energy/defectformationenergy.py
+++ MAST/utility/defect_formation_energy/defectformationenergy.py
@@ -23,11 +23,9 @@ class DefectFormationEnergy:
             if (ingredient.children is None):
                 self.final_ingredients.append(ingredient.name)
 
-        print self.final_ingredients
-
     def calculate_defect_formation_energies(self):
-        perf_dir = self.get_perfect_directory()[0]
-        def_dir = self.get_defect_directories()
+        perf_dir = [ingredient for ingredient in self.final_ingredients if ('perfect' in ingredient)][0]
+        def_dir = [ingredient for ingredient in self.final_ingredients if 'perfect' not in ingredient] 
 
         defects = self.input_options.get_item('defects', 'defects')
         chempot = self.input_options.get_item('chemical_potentials')
@@ -38,13 +36,10 @@ class DefectFormationEnergy:
         for string in system_name[1:-1]:
             name += ('_' + string)
 
-        #print perf_dir
-        #print defects
-        #print chempot
-        #print name
-
         e_perf = self.get_total_energy(perf_dir)
+        efermi = self.get_fermi_energy(perf_dir)
         struct_perf = self.get_structure(perf_dir)
+
         perf_species = dict()
         for site in struct_perf:
             if (str(site.specie) not in perf_species):
@@ -55,46 +50,64 @@ class DefectFormationEnergy:
         #print 'Perfect composition: ', perf_species
         e_defects = dict() # dict of defect energies
 
-        # I want this to be the other way around (i.e. loop through conditions then defects
-        # For now I will leave it like this.
-        for label, defect in defects.items():
-            def_dir = '%s/%s_%s_q=p0_sp' % (self.directory, name, label)
+        # First loop through the conditions for the defects
+        for conditions, potentials in chempot.items():
+            print 'Conditions:  %s' % conditions.upper()
             defect_info = dict()
 
-            energy = self.get_total_energy(def_dir)
-            structure = self.get_structure(def_dir)
-            def_species = dict()
-            for site in structure.sites:
-                if (site.specie not in def_species):
-                    def_species[site.specie] = 1
-                else:
-                    def_species[site.specie] += 1
+            # Loop through each defect
+            for label, defect in defects.items():
+                print 'Calculating DFEs for defect %s' % (label)
 
-            struct_diff = dict()
-            for specie, number in def_species.items():
-                try:
-                    nperf = perf_species[str(specie)]
-                except KeyError:
-                    nperf = 0
-                #print number - nperf
-                struct_diff[str(specie)] = number - nperf
+                # List of directories pertaining to a certain defect
+                ddir = [ddir for ddir in def_dir if label in ddir]
 
-            base = energy - e_perf # E_defect - E_perf
+                # Now loop through the charges for each defect
+                energy_list = list()
+                for charged in ddir:
+                    charge = charged.split('_q=')[-1].split('_')[0]
+                    sign = charge[0]
 
-            defect_info = dict()
-            for conditions, potentials in chempot.items():
-                print 'DFE for defect %s under %s conditions' % (label, conditions.upper())
-                #print '%-20s = %18.6f' % ('Energy defected', energy)
-                #print '%-20s = %18.6f' % ('Energy perfect', e_perf)
-                e_def = base
-                for specie, number in struct_diff.items():
-                    mu = potentials[str(specie).lower()]
-                    #print str(specie), mu, number
-                    e_def -= (number * mu)
-                defect_info[conditions] = e_def
-                print 'DFE =', e_def
+                    if (sign == 'p'):
+                        charge = int(charge[1:])
+                    elif (sign == 'n'):
+                        charge = -1 * int(charge[1:])
 
-            e_defects[label] = defect_info
+                    energy = self.get_total_energy(charged)
+                    structure = self.get_structure(charged)
+
+                    # Find out how many atoms of each type are in the defects
+                    def_species = dict()
+                    for site in structure.sites:
+                        if (site.specie not in def_species):
+                            def_species[site.specie] = 1
+                        else:
+                            def_species[site.specie] += 1
+
+                    # Find the differences in the number of each atom type
+                    # between the perfect and the defect
+                    struct_diff = dict()
+                    for specie, number in def_species.items():
+                        try:
+                            nperf = perf_species[str(specie)]
+                        except KeyError:
+                            nperf = 0
+                        struct_diff[str(specie)] = number - nperf
+
+                    # Calculate the base DFE energy
+                    e_def = energy - e_perf # E_defect - E_perf
+                    for specie, number in struct_diff.items():
+                        mu = potentials[str(specie).lower()]
+                        #print str(specie), mu, number
+                        e_def -= (number * mu)
+                    e_def += charge * (efermi) # Add in the shift here!
+                    energy_list.append([charge, e_def])
+
+                energy_list = sorted(energy_list, key=lambda energy: energy[0])
+                defect_info[label] = energy_list
+
+            e_defects[conditions] = defect_info
+
         return e_defects
 
     def get_defect_directories(self):
@@ -106,11 +119,34 @@ class DefectFormationEnergy:
     def get_total_energy(self, directory):
         """Returns the total energy from a directory"""
         # Are we dealing with VASP?
-        if ('vasprun.xml' in os.listdir(directory)):
+        abspath = '%s/%s/' % (self.directory, directory)
+        if ('vasprun.xml' in os.listdir(abspath)):
         # Modified from the PyMatGen Vasprun.final_energy() function to return E_0
-            return Vasprun('%s/vasprun.xml' % directory).ionic_steps[-1]["electronic_steps"][-1]["e_0_energy"]
+            return Vasprun('%s/vasprun.xml' % abspath).ionic_steps[-1]["electronic_steps"][-1]["e_0_energy"]
+
+    def get_fermi_energy(self, directory):
+        """Returns the Fermi energy from a directory"""
+        abspath = '%s/%s/' % (self.directory, directory)
+        if ('vasprun.xml' in os.listdir(abspath)):
+        # Modified from the PyMatGen Vasprun.final_energy() function to return E_0
+            return Vasprun('%s/vasprun.xml' % abspath).efermi
 
     def get_structure(self, directory):
         """Returns the final structure from an optimization"""
-        if ('vasprun.xml' in os.listdir(directory)):
-            return Vasprun('%s/vasprun.xml' % directory).final_structure
+        abspath = '%s/%s/' % (self.directory, directory)
+        if ('vasprun.xml' in os.listdir(abspath)):
+            return Vasprun('%s/vasprun.xml' % abspath).final_structure
+
+    def print_table(self):
+        e_defects = self.calculate_defect_formation_energies()
+
+        for conditions, defects in e_defects.items():
+            print '\n\nDefect formation energies for %s conditions.\n' % conditions.upper()
+            print '%-20s | %10s' % ('Defect', 'DFE')
+            print '---------------------------------'
+            for defect, energies in defects.items():
+                for energy in energies:
+                    print '%-14s(q=%2i) | %8.4f' % (defect, energy[0], energy[1])
+                print str() # Add a blank line here
+            print '---------------------------------'
+
