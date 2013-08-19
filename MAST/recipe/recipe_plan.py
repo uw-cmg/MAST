298523b4a8a29c533df889f244e2afeac6d948a1
Tam Mayeshiba <mayeshiba@wisc.edu>
2013-08-19 18:01:50 -0500

    Improved recipe plan to adopt more recipe-level functionality instead of having it handled by the DAG

--- MAST/recipe/recipe_plan.py
+++ MAST/recipe/recipe_plan.py
@@ -7,7 +7,7 @@
 # Replace this section with appropriate license text before shipping.
 # Add additional programmers and schools as necessary.
 ############################################################################
-
+from MAST.ingredients.chopingredient import ChopIngredient
 class RecipePlan:
     """Contains the entire recipe plan. It consists of
        - Ingredients Objects
@@ -15,10 +15,119 @@ class RecipePlan:
     """
     def __init__(self, name):
         self.name            = name
-        self.ingredients     = dict()
-        self.how_to_update   = dict()
+        self.ingredients     = dict()  #name=status
+        self.update_methods   = dict()
         self.parents_to_check= dict()
+        self.run_methods      = dict()
+        self.write_methods    = dict()
+        self.ready_methods    = dict()
+        self.complete_methods = dict()
+        self.ingred_input_options = dict()
         #print 'GRJ DEBUG: Initializing RecipePlan'
+
+    def write_ingredient(iname):
+        """Write the ingredient files according to the 
+            correct method
+        """
+        methodname = self.write_methods[iname]
+        my_ing = ChopIngredient(self.ingred_input_options[iname])
+        writeresult=getattr(ChopIngredient, methodname)(my_ing)
+        return writeresult
+
+    def complete_ingredient(iname):
+        """Check if an ingredient is complete
+        """
+        methodname = self.complete_methods[iname]
+        my_ing = ChopIngredient(self.ingred_input_options[iname])
+        iscomplete=getattr(ChopIngredient, methodname)(my_ing)
+        return iscomplete
+
+    def ready_ingredient(iname):
+        """Check if an ingredient is ready
+        """
+        methodname = self.ready_methods[iname]
+        my_ing = ChopIngredient(self.ingred_input_options[iname])
+        isready=getattr(ChopIngredient, methodname)(my_ing)
+        return isready
+
+
+    def run_ingredient(iname):
+        """Run ingredient
+        """
+        methodname = self.run_methods[iname]
+        my_ing = ChopIngredient(self.ingred_input_options[iname])
+        runresult=getattr(ChopIngredient, methodname)(my_ing)
+        return runresult
+
+    def update_children(iname):
+        """Update the children of an ingredient
+        """
+        upd_results=list()
+        for childname in self.update_methods[iname]:
+            methodname = self.update_methods[iname][childname]
+            my_ing = ChopIngredient(self.ingred_input_options[iname])
+            updresult=getattr(ChopIngredient, methodname)(my_ing)
+            upd_results.append(updresult)
+        return upd_results
+
+    def check_if_queued_are_complete():
+        """Check if queued ingredients are complete
+        """
+        for iname in self.ingredients.keys():
+            if self.ingredient[iname] == "Q":
+                if self.complete_ingredient(iname):
+                    self.ingredient[iname] = "C"
+                    self.update_children(iname)
+        return
+    
+    def check_if_parents_are_complete():
+        """Check if parents of waiting ingredients are
+            complete.
+        """
+        for iname in self.ingredients.keys():
+            if self.ingredient[iname] == "W":
+                okay=0
+                ptc = list(self.parents_to_check[iname])
+                plen = len(ptc)
+                for parent in ptc:
+                    if self.ingredients[parent] == "C":
+                        okay = okay + 1
+                if okay == plen:
+                    self.ingredients[iname] = "S"
+
+    def run_staged_ingredients():
+        """Run staged ingredients.
+        """
+        for iname in self.ingredients.keys():
+            if self.ingredient[iname] == "S":
+                self.write_ingredient(iname)
+                if self.ready_ingredient(iname):
+                    self.run_ingredient(iname)
+                    self.ingredient[iname] = "Q"
+
+
+    def check_recipe_status():
+        """Check ingredient statuses, and get recipe status
+            W = waiting on parents
+            I = parents complete, okay to stage
+            Q = queued
+            C = complete
+        Return:
+            C = complete
+            R = running
+        """
+        total = len(self.ingredients.keys())
+        self.check_if_queued_are_complete()
+        self.check_if_parents_are_complete()
+        self.run_staged_ingredients()
+        
+        totcomp=0
+        for iname in self.ingredients():
+            if self.ingredients[iname] == "C":
+                totcomp = totcomp + 1
+        if totcomp == total:
+            return True
+        return False
 
     def add_ingredient(self, ingredient_name, ingredient):
         """Used to add an ingredient_object corresponding to an ingredient name
