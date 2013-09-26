bf1581ddf7f2fa73d177fdaafc768bca077f58f8
cmgtam <mayeshiba@wisc.edu>
2013-09-26 14:46:37 -0500

    Revert "moving files around. Difference btw structure and structure file"
    This reverts commit f089abbdfcde4a9518d4f29c492498b9be952a82.

--- MAST/ingredients/checker/__init__.py
+++
@@ -1,3 +0,0 @@
-from MAST.ingredients.checker.basechecker import BaseChecker
-from MAST.ingredients.checker.vaspchecker import VaspChecker
-from MAST.ingredients.checker.phonchecker import PhonChecker
