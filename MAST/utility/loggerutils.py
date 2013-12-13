24ee82741364111756071a43598708f53920419e
cmgtam <mayeshiba@wisc.edu>
2013-12-13 14:36:53 -0600

    error handling format change

--- MAST/utility/loggerutils.py
+++ MAST/utility/loggerutils.py
@@ -4,7 +4,7 @@ def initialize_logger(filename="default.log"):
     logger     = logging.getLogger(filename)
     if not getattr(logger, 'handler_set', None):
         handler    = logging.FileHandler(filename)
-        formatter  = logging.Formatter('%(asctime)s : <%(pathname)s:%(lineno)d> : %(levelname)s : %(message)s')
+        formatter  = logging.Formatter('%(asctime)s : %(module)15s:%(lineno)4d> : %(levelname)8s : %(message)s')
         handler.setFormatter(formatter)
         logger.addHandler(handler)
         logger.setLevel(logging.INFO)
