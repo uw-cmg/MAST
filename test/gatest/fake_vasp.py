4429ce2224f44e33ecfa8129623be9747b99f0d8
cmgtam <mayeshiba@wisc.edu>
2014-03-05 13:21:40 -0600

    Modify fake-vasp to more realistic output for testing

--- test/gatest/fake_vasp.py
+++ test/gatest/fake_vasp.py
@@ -2,6 +2,7 @@
 import numpy as np
 import time
 import pymatgen
+import ase
 timestamp = time.asctime()
 randenergy = np.random.random_sample()*-100.0 + -200.0
 randtime = np.random.random_sample()*100
@@ -10,13 +11,25 @@ mystruc = pymatgen.io.vaspio.Poscar.from_file("POSCAR").structure
 mystruc.perturb(0.01)
 fake_contcar = pymatgen.io.vaspio.Poscar(mystruc)
 fake_contcar.write_file("CONTCAR")
+#
+from ase.calculators.lj import LennardJones
+calc = LennardJones()
+aseatoms = ase.io.read("CONTCAR")
+aseatoms.set_calculator(calc)
+aseenergy = aseatoms.get_potential_energy()
+stress_tensor = aseatoms.get_stresses()
+asepress = aseatoms.get_isotropic_pressure(stress_tensor)
+
 fake_osz = open("OSZICAR","wb")
 fake_osz.write("Output randomly generated at %s\n" % timestamp)
-fake_osz.write("E0=% 3.8f   d E =0.00\n" % randenergy)  
+#fake_osz.write("E0=% 3.8f   d E =0.00\n" % randenergy)  
+fake_osz.write("E0=% 3.8f   d E =0.00\n" % aseenergy)  
 fake_osz.close()
+
 fake_outcar = open("OUTCAR","wb")
 fake_outcar.write("Output randomly generated at %s\n" % timestamp)
-fake_outcar.write("external pressure =   %3.8f kB  Pullay stress =  0.00 kB\n" % randpress)
+#fake_outcar.write("external pressure =   %3.8f kB  Pullay stress =  0.00 kB\n" % randpress)
+fake_outcar.write("external pressure =   %3.8f kB  Pullay stress =  0.00 kB\n" % asepress)
 fake_outcar.write("Pretend we have...reached required accuracy\n")
 fake_outcar.write("User time (sec): %3.8f" % randtime)
 fake_outcar.close()
