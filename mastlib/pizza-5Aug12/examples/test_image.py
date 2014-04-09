9ecef98cca86d70e2e36c5aa42b8d73cf47b10fa
cmgtam <mayeshiba@wisc.edu>
2014-04-09 15:11:41 -0500

    Remove pizza.py from mastlib. May not redistribute.

--- mastlib/pizza-5Aug12/examples/test_image.py
+++
@@ -1,9 +0,0 @@
-# simple test of image tool
-# requires files/bucky*.png
-
-i = image("files/bucky*.gif")
-i.convert("files/bucky*.gif","tmp*.png")
-i.montage("","files/bucky*.gif","tmp*.png","tmpnew*.gif")
-i.view("*.gif")
-
-print "all done ... type CTRL-D to exit Pizza.py"
