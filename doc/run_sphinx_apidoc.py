a37eac5aa98d936c9c45dea67405b3a36c04b16b
cmgtam <mayeshiba@wisc.edu>
2014-04-25 20:42:41 -0500

    Added header to doc; but not to doc/source

--- doc/run_sphinx_apidoc.py
+++ doc/run_sphinx_apidoc.py
@@ -1,3 +1,9 @@
+##############################################################
+# This code is part of the MAterials Simulation Toolkit (MAST)
+# 
+# Maintainer: Tam Mayeshiba
+# Last updated: 2014-04-25
+##############################################################
 import os
 #  cmd = "sphinx-apidoc --separate --force -o ./source %s" % os.environ['MAST_INSTALL_PATH']
 cmd = "sphinx-apidoc --separate -o ./source %s" % os.environ['MAST_INSTALL_PATH']
