dbc4ff9014ba0586a2f9db72cd5821334a5a15f5
cmgtam <mayeshiba@wisc.edu>
2014-04-30 10:22:55 -0500

    Update documentation, add warning for running apidoc

--- doc/run_sphinx_apidoc.py
+++ doc/run_sphinx_apidoc.py
@@ -4,6 +4,7 @@
 # Maintainer: Tam Mayeshiba
 # Last updated: 2014-04-25
 ##############################################################
+raise NotImplementedError("Comment out this line if you are sure you want to run this file! You may want to delete initialize.rst before running make latexpdf or make html")
 import os
 #  cmd = "sphinx-apidoc --separate --force -o ./source %s" % os.environ['MAST_INSTALL_PATH']
 cmd = "sphinx-apidoc --separate -o ./source %s" % os.environ['MAST_INSTALL_PATH']
