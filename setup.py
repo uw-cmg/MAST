5b2f89f1efa777bf704b00271f70fd6b5f9931a2
cmgtam <mayeshiba@wisc.edu>
2014-05-08 16:02:05 -0500

    Changes to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 #TTM use_setuptools()
 from setuptools.command.install import install
 from setuptools import setup, find_packages
+from distutils.command.build_py import build_py as _build_py
 
 #python version check
 import sys
@@ -10,13 +11,29 @@ if sys.version_info[0] < 2 or (sys.version_info[0] == 2 and sys.version_info[1] 
     print "Python version >= 2.7.3 needed!!!"
     sys.exit(0)
 
+def run_initialize(my_platform):
+    import subprocess
+    myfile = open("setup_output","wb")
+    myinit = subprocess.Popen("mast_initialize", shell=True, stdout=myfile, stderr=myfile)
+    myinit.wait()
+    myfile.close()
+    return
+
+class build_py(_build_py):
+    """Specialized Python source builder."""
+    print "Hello!"
+    my_platform = raw_input("Platform?: ")
+    run_initialize(my_platform)
+
 setup(
-        name="MAST",
+        name="MAST_tam_test",
         packages=find_packages(),
-        version="1.0.19",
+        version="1.0.13",
         #setup_requires=["numpy>=1.6.1"],
         install_requires=["numpy>=1.6.1", "scipy>=0.10.1", "pymatgen>=2.8.8", "custodian>=0.5.1"],
-        #scripts=["MAST/initialize"],
+        scripts=["MAST/bin/mast","MAST/mast_initialize"],
+
+
         author="MAST Development Team, University of Wisconsin-Madison Computational Materials Group",
         author_email="ddmorgan@wisc.edu",
         #maintainer="Tam Mayeshiba",
@@ -36,7 +53,8 @@ setup(
             "Topic :: Scientific/Engineering :: Chemistry",
             "Topic :: Software Development :: Libraries :: Python Modules"
         ],
-        data_files=[
-            ("MAST/recipe_templates",["recipe_templates/neb_with_phonons.txt"])
-        ],
+        #data_files=[
+        #    ("MAST/recipe_templates",["MAST/recipe_templates/neb_with_phonons.txt"])
+        #],
+        cmdclass={'build_py': build_py}
 )
