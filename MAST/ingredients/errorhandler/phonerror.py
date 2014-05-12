faf7677191afda83963b34b51309071fe7b33427
Zhewen Song <zsong39@wisc.edu>
2014-05-12 12:32:34 -0500

    Create phonerror.py

---
+++ MAST/ingredients/errorhandler/phonerror.py
@@ -0,0 +1,22 @@
+##############################################################
+# This code is part of the MAterials Simulation Toolkit (MAST)
+# PHON error handler is obsolete, as PHON is no longer being supported.
+# Maintainer: Tam Mayeshiba
+# Last updated: 2014-04-25
+##############################################################
+from custodian.vasp import handlers
+from pymatgen.core.structure import Structure
+import inspect
+import os
+import logging
+from MAST.ingredients.errorhandler import BaseError
+class PhonError(BaseError):
+    """PHON error-handling functions 
+    """
+    def __init__(self, **kwargs):
+        allowed_keys = {
+            'name' : (str, str(), 'Name of directory'),
+            'program_keys': (dict, dict(), 'Dictionary of program keywords'),
+            'structure': (Structure, None, 'Pymatgen Structure object')
+            }
+        BaseError.__init__(self, allowed_keys, **kwargs)
