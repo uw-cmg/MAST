3ccce08d2a636b234a8e9377bc714aa4128d5277
weixie4 <weixie4@gmail.com>
2014-05-26 13:10:25 -0400

    update fss modules to latest versions

--- MAST/utility/finite_size_scaling/EneVsVm.py
+++ MAST/utility/finite_size_scaling/EneVsVm.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python
 ##############################################################
 # This code is part of the MAterials Simulation Toolkit (MAST)
 # 
 # Maintainer: Wei Xie
-# Last updated: 2014-04-25
+# Last updated: 2014-04-19
 ##############################################################
 import sys, getopt, os
 import shutil
@@ -14,7 +15,16 @@ import pymatgen as mg
 from pymatgen.analysis import ewald
 from pymatgen.io import vaspio
 
+import GenSC
 
+class writer :
+        def __init__(self, *writers) :
+                self.writers = writers
+
+        def write(self, text) :
+                for w in self.writers :
+                        w.write(text)
+                        
 def psTotEne(oszicar):
     dummyOszcar = vaspio.vasp_output.Oszicar(oszicar).electronic_steps
     TotEne = dummyOszcar[len(dummyOszcar)-1][len(dummyOszcar[len(dummyOszcar)-1])-1]['E']
@@ -24,65 +34,221 @@ def CalcV_M(structure):
     dummyPoscarLatStr = mg.core.structure.Structure(structure.lattice,["F-"],[[0,0,0]])
     V_M = ewald.EwaldSummation(dummyPoscarLatStr).total_energy*(-2)
     return V_M
+
+
+        
+def collectEformV_M(perfectDir,defectDirs,defectChg,mu_e,mu_i):
+    #mu_e is the chemical potential of electron 
+    #mu_i is a dictionary of chemical potentials for atomic species
     
-def FitnPlot(listX,listY,Xlabel,Ylabel,plotBoolean):
+    Eform = [None]*len(defectDirs)
+    V_M = [None]*len(defectDirs)
+    
+    prf=mg.io.vaspio.vasp_output.Vasprun(perfectDir+"_vasprun.xml")
+
+    prf_ene=prf.final_energy
+    #TotEnePr = psTotEne("OSZICAR")
+
+    prf_strct=prf.structures[len(prf.structures)-1]
+    
+    prf_comps=prf_strct.composition.element_composition
+    #perfect_comps = mg.io.vaspio.Poscar.from_file("CONTCAR").structure.composition    
+    prf_natom = prf_comps.num_atoms
+    
+    prf_elmnts = prf_comps.elements
+    
+    #gap,cbm,vbm,ifdirectgap=prf.eigenvalue_band_properties
+    #print (defectDirs[i]+" "+str(V_M[i])+" "+str(gap)+" "+str(cbm)+" "+str(vbm))
+   
+    all_elmnts = prf_elmnts               
+    for i in range(len(defectDirs)):
+        
+        dfct=mg.io.vaspio.vasp_output.Vasprun(defectDirs[i]+"_vasprun.xml")
+
+        dfct_strct=dfct.structures[len(dfct.structures)-1]
+        
+        V_M[i] = CalcV_M(dfct_strct)
+
+        dfct_comps=dfct_strct.composition.element_composition    
+        #defct_comps=defct_strct.composition             
+
+        dfct_natom = dfct_comps.num_atoms
+        
+        dfct_ene = dfct.final_energy
+        #defct_ene = psTotEne("OSZICAR")
+        
+        scalingF = round(dfct_natom/prf_natom)
+                    
+        dfct_elmnts = dfct_comps.elements
+        #print dfct_elmnts
+        
+        if i == 0:
+
+            for dummy_elmnt1 in dfct_elmnts:
+                if dummy_elmnt1 not in all_elmnts:
+                    all_elmnts.append(dummy_elmnt1)
+
+            all_amnt_change=[None]*len(all_elmnts) 
+            
+            for j, dummy_elmnt2 in enumerate(all_elmnts):
+                if dummy_elmnt2 in prf_elmnts:
+                    dummy_prf_amnt = prf_comps.__getitem__(dummy_elmnt2)
+                else:
+                    dummy_prf_amnt = 0
+                if dummy_elmnt2 in dfct_elmnts:
+                    dummy_dfct_amnt = dfct_comps.__getitem__(dummy_elmnt2)
+                else:
+                    dummy_dfct_amnt = 0
+                
+                all_amnt_change[j]=scalingF*dummy_prf_amnt-dummy_dfct_amnt                                    
+
+        Eform[i] =  dfct_ene - scalingF*prf_ene 
+
+        #Eform[i] += defectChg*(vbm+mu_e*gap)
+        Eform[i] += defectChg*mu_e
+
+        for k in range(len(all_elmnts)):
+
+            if all_amnt_change[k] != 0:
+
+                if all_elmnts[k].symbol in mu_i:                    
+                    Eform[i] += mu_i[all_elmnts[k].symbol]*all_amnt_change[k]
+                else:
+                    raise RuntimeError("Chemical potential of "+all_elmnts[k].symbol
+                                        +"is not provided!")       
+
+    return(V_M,Eform)
+
+
+def linearFit(listX,listY):
     listXnp = np.array(listX)
     listYnp = np.array(listY)
     slope, intercept, r_value, p_value, std_err = stats.linregress(listXnp,listYnp)
-
-    print 'slope: ', slope
-    print 'intercept: ', intercept
-    print 'correlation coefficent r: ', r_value
-    print 'standard deviation: ', std_err
     
-    if plotBoolean:
-        listYfitted = slope*listXnp + intercept
-        fig = plt.figure()
-        ax = fig.add_axes([0.12,0.1,0.8,0.8])
-        plt.plot(listXnp,listYfitted,'r-',listXnp,listYnp,'o')
-        plt.xlabel(Xlabel)
-        plt.ylabel(Ylabel)
+    print "The results of linear fitting:"
+    print "slope: ", slope
+    print "intercept: ", intercept
+    print "correlation coefficent r: ", r_value
+    print "standard deviation: ", std_err
+    print "" 
+    return [slope,intercept,r_value,std_err]   
+    
+def plotFit(listX,listY,Xlabel,Ylabel,slpIntcpt,figTitle,dirNames):
+    listXnp = np.array(listX)
+    listYnp = np.array(listY)
+    listYnpCorrectted = listYnp-slpIntcpt[0]*listXnp
 
-        #fittedEq = (Ylabel + " = " + str(slope) + " * " + Xlabel + " + " + str(intercept))        
-        fittedEq = ("Y = (" + str(slope) + ") * X" + " + (" + str(intercept) + ")")
-        #fig.text(0.35, 0.75, str('intercept is'+' '+str(intercept)))
-        fig.text(0.35, 0.75, fittedEq)
+    Left=min(listX)-0.2
+    Right=max(listX)+0.2
         
-        plt.show()    
-        
+    listXnpFitted=np.arange(Left,Right,(Right-Left)/6)
+    listXnpFitted=np.append(listXnpFitted,Right)
+    listYnpFitted = slpIntcpt[0]*listXnpFitted + slpIntcpt[1]
+    listYnpCorrecttedFitted = listXnpFitted*0+slpIntcpt[1]
+    
+    fig = plt.figure()
+    ax = fig.add_axes([0.12,0.1,0.8,0.8])
+    
+    #plt.plot(listXnp,listYnp,'ro',listXnpFitted,listYnpFitted,'r-',
+    #         listXnp,listYnpCorrectted,'bo',listXnpFitted,listYnpCorrecttedFitted,'b--')
+    line1=plt.plot(listXnp,listYnp,'ro',label='original')
+    plt.hold(True)
+    line2=plt.plot(listXnpFitted,listYnpFitted,'r-')
+    line3=plt.plot(listXnp,listYnpCorrectted,'bo',label='corrected')
+    line4=plt.plot(listXnpFitted,listYnpCorrecttedFitted,'b--')
+    listXnpFitted,listYnpFitted,'r-',
+    plt.hold(False) 
+    
+    plt.xlabel(Xlabel)
+    plt.ylabel(Ylabel)
+
+    plt.legend()
+
+    plt.xlim(Left,Right)
+    
+    dummyBottom=min(min(listYnp),min(listYnpCorrectted))
+    dummyTop=max(max(listYnp),max(listYnpCorrectted))
+    Bottom=dummyBottom-0.2*(dummyTop-dummyBottom)
+    Top=dummyTop+0.2*(dummyTop-dummyBottom)
+    plt.ylim(Bottom,Top)
+
+    plt.axvline(x=0,color='b',ls='dashed')
+    
+    #fittedEq = (Ylabel + " = " + str(slope) + " * " + Xlabel + " + " + str(intercept))
+    #fittedEq = ("Y = (" + str(slpIntcpt[0]) + ") * X" + " + (" + str(slpIntcpt[1]) + ")")
+    #fittedEq = ("Slope is " + str(round(slpIntcpt[0],2)) + 
+    #            ". Intercept is " + str(round(slpIntcpt[1],2))+".")
+    #fig.text(0.35, 0.25, fittedEq) 
+    
+    fig.text(0.14,0.25,("Slope: " + str(round(slpIntcpt[0],2))))       
+    fig.text(0.14,0.21,("Intercept: " + str(round(slpIntcpt[1],2))))
+    fig.text(0.14,0.17,("Correlation coefficent r: " + str(round(slpIntcpt[2],2))))       
+    fig.text(0.14,0.13,("Standard deviation: " + str(round(slpIntcpt[3],2)))) 
+    
+    for i, dirName in enumerate(dirNames):
+        fig.text(0.12+0.8*(listX[i]-Left)/(Right-Left)-0.03,
+                 0.10+0.8*(listY[i]-Bottom)/(Top-Bottom)+0.01,dirName)       
+    
+    fig.suptitle(figTitle, fontsize=12)
+                          
+    plt.show()    
 
 if __name__ == "__main__":
-    perfectDir = "perfect_30atoms"
-    Dirs = ["2x2x1","2x2x2","2x2x3","3x3x1","4x4x1","3x3x2"]
+    
+    #plotTitle='VO2+,O rich, mu_e=VBM'
+    #perfectDir =  "../primordial_perfect/8x8x4_kmesh"
+    #defectDirs = ["2x2x1","2x2x2","2x2x3","3x3x1","3x3x2","4x4x1"]
+    #mu_e=6.268
+    #mu_i={'Al':-3.74,'O':-4.20}
 
-    FormEneDef = [None]*len(Dirs)
-    V_M = [None]*len(Dirs)
+    plotTitle='VO2+,O rich, mu_e=VBM'
+    perfectDir = "../../primordial_perfect/12x12x12_kmesh"
+    defectDirs = ["1x2x2","1x2x3","1x3x4","2x2x2","2x3x4","3x3x3"]   
+    mu_e=4.999
+    mu_i={'Mg':-7.40,'O':-4.57}
 
-    cwdir = os.getcwd()
-
-    os.chdir(perfectDir)
-    TotEnePr = psTotEne("OSZICAR")
-    NatomPr = mg.io.vaspio.Poscar.from_file("POSCAR").structure.composition.num_atoms
-    os.chdir(cwdir)
-
-    for i in range(len(Dirs)):
-        os.chdir(Dirs[i])
-
-        dummyTotEneSc = psTotEne("OSZICAR")
-        dummmyNatomSc = mg.io.vaspio.Poscar.from_file("POSCAR").structure.composition.num_atoms
-        dummyScalF = round(dummmyNatomSc/NatomPr)
-        #print dummyScalF
-        FormEneDef[i] =  dummyTotEneSc - dummyScalF*TotEnePr
+    #plotTitle='VSi2-,C rich, mu_e=VBM'
+    #perfectDir = "../primordial_perfect/12x12x12_kmesh"
+    #defectDirs = ["2x2x2","2x2x3","2x3x3","3x3x3"]
+    #defectDirs = ["2x2x2","2x2x3","2x2x4","2x3x3","3x3x3"]
+    #mu_e=8.423
+    #Si rich
+    #mu_i={'Si':-5.44,'C':-9.65}
+    #C rich
+    #mu_i={'Si':-5.89,'C':-9.20}
+    #Si/O equally rich
+    #mu_i={'Si':-5.67,'C':-9.43}    
         
-        #V_M[i] = CalcV_M("CONTCAR")
-        V_M[i] = CalcV_M(mg.io.vaspio.Poscar.from_file("CONTCAR").structure)      
+    saved = sys.stdout
+    fout = file('out.log', 'w')
+    sys.stdout = writer(sys.stdout, fout)
     
-        print (Dirs[i]+" "+str(V_M[i])+" "+str(FormEneDef[i]))
+    sc_struct=mg.io.vaspio.Poscar.from_file(defectDirs[0]+"_CONTCAR").structure
+    sc_potcar=mg.io.vaspio.Potcar.from_file(defectDirs[0]+"_POTCAR")
+    sc_incar=mg.io.vaspio.Incar.from_file(defectDirs[0]+"_INCAR") 
+    defchg=GenSC.defChg(sc_struct,sc_potcar,sc_incar)
+    print "defect charge: ", defchg
+            
+    (V_M,Eform_orig)=collectEformV_M(perfectDir,defectDirs,defchg,mu_e,mu_i)
+
+    slpintcpt=linearFit(V_M,Eform_orig) 
+    FS_corr=-np.array(V_M)*slpintcpt[0]
+    Eform_corr=np.array(Eform_orig)+FS_corr
+
+    print ("Folder " " V_M " "  EForm_orig" "  EForm_corrct " " FSCorrection")
+    print ("-----------------------------------------------------")
+    for j in range(len(defectDirs)):
+        print (defectDirs[j]+"   "+
+               str(round(V_M[j],2))+"   "+
+               str(round(Eform_orig[j],5))+"    "+
+               str(round(Eform_corr[j],5))+"       "+
+               str(round(FS_corr[j],5)))    
+    print ""       
+
+    dielectConst=-defchg**2/2/slpintcpt[0]
+    print "effective permittivity epsilon_fit: ", round(dielectConst,2)
+
+    plotFit(V_M,Eform_orig,'Madelung Potential v_M (eV)','Formation Energy (eV)',slpintcpt,plotTitle,defectDirs)
     
-        os.chdir(cwdir)
-
-    print ""
-    FitnPlot(V_M,FormEneDef,'Madelung Potential V_M (eV)','Formation Energy (eV)',True)
-    
-
-
+    sys.stdout = saved
+    fout.close()
--- MAST/utility/finite_size_scaling/GenSC.py
+++ MAST/utility/finite_size_scaling/GenSC.py
@@ -1,40 +1,69 @@
+#!/usr/bin/env python
 ##############################################################
 # This code is part of the MAterials Simulation Toolkit (MAST)
 # 
 # Maintainer: Wei Xie
-# Last updated: 2014-04-25
+# Last updated: 2014-04-19
 ##############################################################
 import sys
 import getopt 
 import os
 import shutil
 import errno
-#import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 
 import pymatgen as mg
 from pymatgen.analysis import ewald
 from pymatgen.io import vaspio
-import EneVsVm
-from MAST.utility import MASTFile
 
-def roof_mean(intA,intB):
-    if (intA+intB)%2 == 0:
-        return (intA+intB)/2
-    else:
-        return (intA+intB+1)/2
+import EneVsVm     
+
+class writer:
+        def __init__(self, *writers) :
+                self.writers = writers
+
+        def write(self, text) :
+                for w in self.writers :
+                        w.write(text)
 
 def mkdir_p(path):
     try:
         os.makedirs(path)
-    except OSError as exc: # Python >2.5
+    except OSError as exc: 
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
-        else: raise
-        
-               
+        else:
+            raise      
+def num_atoms_speciewise(inputstruct):
 
+    transitionSiteIndex=[0]
+    SpecieLastSite=inputstruct.species[0]
+    for i, specieThisSite in enumerate(inputstruct.species):
+        if specieThisSite != SpecieLastSite:
+            transitionSiteIndex.append(i)
+        SpecieLastSite=specieThisSite
+    numSpecieTypes=len(transitionSiteIndex)
+    numAtomsSpeciewise=[None]*numSpecieTypes
+    for j in range(numSpecieTypes-1):
+        numAtomsSpeciewise[j]=transitionSiteIndex[j+1]-transitionSiteIndex[j]
+    numAtomsSpeciewise[numSpecieTypes-1]=(int(inputstruct.composition.num_atoms)-
+                                        transitionSiteIndex[numSpecieTypes-1])
+
+    return numAtomsSpeciewise           
+    
+def defChg(inputStruct,inputPotcar,inputIncar):
+    if ('NELECT' not in inputIncar): 
+        raise RuntimeError("cannot find NELECT in input INCAR")
+    natoms_el=num_atoms_speciewise(inputStruct)
+    zval_el=[None]*len(inputPotcar)
+    defchg=0
+    for i in range(len(inputPotcar)):
+        zval_el[i]=inputPotcar[i].ZVAL
+        defchg=defchg+zval_el[i]*natoms_el[i]
+    defchg=defchg-inputIncar['NELECT']
+    return defchg   
+                        
 def minVertDist(inputlat):
     dummyMat = inputlat.matrix
     return min(np.linalg.norm(dummyMat[0]),
@@ -51,105 +80,230 @@ def minVertDist(inputlat):
                np.linalg.norm(dummyMat[0]-dummyMat[1]+dummyMat[2]),
                np.linalg.norm(dummyMat[0]-dummyMat[1]-dummyMat[2])
                )
+
+def roof_mean(intA,intB):
+    if (intA+intB)%2 == 0:
+        return (intA+intB)/2
+    else:
+        return (intA+intB+1)/2
+
                
-def genLMN(primordial,minDefDist,maxNumAtoms):
-    #LMN_UpLimit=int(maxNumAtoms//primordial.composition.num_atoms)
-    
-#    sc_list_raw=[]
+def list2Bins(inputlist,numBins):
+    '''
+    helper function to group the elements of inputlist into some evenly spaced 
+    subgroups called bins. Each bin has two boundaries and there are totally numBins+1
+    bin boundaries.    
+    '''   
+    listMin=min(inputlist)
+    listMax=max(inputlist)
+
+    if (numBins <= 0):
+        raise RuntimeError("numBins <= 0")
+    elif (listMin == listMax):
+        raise RuntimeError("All memebers of the list have the same value")
+    else:
+        binWidth=(listMax-listMin)/numBins
+
+        binLowerBounds=np.arange(listMin,listMax,binWidth)
+        #binUpperBounds=binLowerBounds+binWidth
+        binBounds=np.append(binLowerBounds,listMax)
+        
+        binLowerBounds=binLowerBounds.tolist()
+        #binUpperBounds=binUpperBounds.tolist()
+        binBounds=binBounds.tolist()     
+
+        #itemsClosest2binBounds=[[] for i in range(len(binBounds))]
+        #itemsClosest2binBounds_index=[[] for i in range(len(binBounds))]
+        #closestDist2binBounds=[[] for i in range(len(binBounds))]
+ 
+        itemsClosest2binBounds=[None]*(len(binBounds))
+        #itemsClosest2binBounds_index=[None]*(len(binBounds))
+        closestDist2binBounds=[None]*(len(binBounds))
+        
+        for i, item in enumerate(inputlist):
+            for j, boundary in enumerate(binBounds):
+                dummyDist=item-boundary
+                if (-binWidth/2<=dummyDist) and (dummyDist<binWidth/2):
+                   if ((itemsClosest2binBounds[j]==None) or 
+                       (abs(dummyDist)<closestDist2binBounds[j])):
+                       itemsClosest2binBounds[j]=inputlist[i]
+                       #itemsClosest2binBounds_index[j]=[i]
+                       closestDist2binBounds[j]=abs(dummyDist)
+                   break                       
+                                              
+    #return (binBounds,itemsClosest2binBounds,closestDist2binBounds)    
+    #return (itemsClosest2binBounds,itemsClosest2binBounds_index)
+    return itemsClosest2binBounds
+
+
+#def genLMNs(primordial_struct,minDefDist=5,maxNumAtoms=600,minNumAtoms=64,numStructAsked=5):
+def genLMNs(primordial_struct,minDefDist=5,maxNumAtoms=600,numStructAsked=5):   
+    maxLMN=int((maxNumAtoms/primordial_struct.composition.num_atoms)**(1/3.0))
+    print ("Generating candidate supercells with the scaling factors"+
+           " L/M/N less than or equal to "+str(maxLMN)+".")
+    print ("If you want to explore larger scaling factors L/M/N, " + 
+           "please rerun with larger maxNumAtoms input parameter.")
+    print (" ")
+    print maxLMN
+    #####The following section generate a list of candidates LMN's######   
     LMN_list_raw=[]
     Vm_list_raw=[]
-    
-    LMN_UpLimit=4
-    for L in range(1,LMN_UpLimit+1):
-            for M in range(1,LMN_UpLimit+1):
-                    for N in range(1,LMN_UpLimit+1):
-                        dummy_sc=primordial.copy()
+    print ("The following are candiate supercells:")   
+    print ("ScalingLMN  " " V_M")
+    print ("-----------------")
+    for L in range(1,maxLMN+1):
+            for M in range(1,maxLMN+1):
+                    for N in range(1,maxLMN+1):
+                        dummy_sc=primordial_struct.copy()
                         dummy_sc.make_supercell([L,M,N])
                         if ((minVertDist(dummy_sc.lattice) >= minDefDist) and 
-                         (dummy_sc.composition.num_atoms <= maxNumAtoms)):                                                       
-                            #sc_list_raw.append(mg.core.structure.IStructure.from_sites(dummy_sc))
-                            #sc_list_raw.append(dummy_sc)
-                            LMN_list_raw.append([L,M,N])
-                            Vm_list_raw.append(EneVsVm.CalcV_M(dummy_sc))
-
-                            
-    if len(LMN_list_raw) <= 5:
+                            #(dummy_sc.composition.num_atoms >= minNumAtoms) and
+                            (dummy_sc.composition.num_atoms <= maxNumAtoms)):
+                            dummy_Vm=np.asscalar(EneVsVm.CalcV_M(dummy_sc))
+                            #print str([L,M,N]),"tmp"                            
+                            alreadyExist=False
+                            for vmraw in Vm_list_raw:
+                                #print str([L,M,N]),"good"
+                                if abs(vmraw-dummy_Vm)<0.00001:
+                                    alreadyExist=True
+                                    break                                                                                     
+                            #if dummy_Vm not in Vm_list_raw:
+                            if not alreadyExist:
+                                LMN_list_raw.append([L,M,N])
+                                Vm_list_raw.append(dummy_Vm)
+                                print str([L,M,N]),"start"
+                                print(str([L,M,N]) +"   "+ str(round(dummy_Vm,2)))
+                                print str([L,M,N]),"finish"  
+    print""   
+    
+    #####The following section select "Good" LMN's from the candidates list######
+    if len(LMN_list_raw) < numStructAsked:
+        LMN_list=LMN_list_raw
+        print ('Warning: You asked for '+str(numStructAsked)+ 
+               ' supercells, but totally only '
+               +str(len(LMN_list_raw))+
+               ' supercells were found to fullfile the input requirements!')
+    elif len(LMN_list_raw) == numStructAsked:
         LMN_list=LMN_list_raw
     else:
         LMN_list=[]
+
         Vm_LMN_dict={}
         for i in range(len(LMN_list_raw)):
-            #print Vm_list_raw[i]
-            #print LMN_list_raw[i]
-            Vm_LMN_dict[Vm_list_raw[i]]=LMN_list_raw[i]
-#            Vm_LMN_dict[str(EneVsVm.CalcV_M(sc_list_raw[i]))]=sc_list_raw[i]
-#            Vm_LMN_dict[EneVsVm.CalcV_M(sc_list_raw[i])]=sc_list_raw[i]
-
-        Vm_list_raw_sorted=sorted(Vm_list_raw)
-        LMN_list.append(Vm_LMN_dict[Vm_list_raw_sorted[0]])
-        LMN_list.append(Vm_LMN_dict[Vm_list_raw_sorted[
-                               roof_mean(0,roof_mean(0,len(Vm_list_raw_sorted)-1))]])
-        LMN_list.append(Vm_LMN_dict[Vm_list_raw_sorted[roof_mean(0,len(Vm_list_raw_sorted))]])
-        LMN_list.append(Vm_LMN_dict[Vm_list_raw_sorted[roof_mean(len(Vm_list_raw_sorted),
-                                                 roof_mean(0,len(Vm_list_raw_sorted)-1))]])
-        LMN_list.append(Vm_LMN_dict[Vm_list_raw_sorted[len(Vm_list_raw_sorted)-1]])
-
-    return LMN_list
-   
+            Vm_LMN_dict[Vm_list_raw[i]]=LMN_list_raw[i]      
         
-def genSCs(primordial,lmn_list):
-    cwdir = os.getcwd()
-    genSCoutput = MASTFile()
-    genSCoutput.data.append(cwdir+"\n")
-    genSCoutput.data.append("----------------------------------\n")
-    genSCoutput.data.append("ScalingLMN "+"    V_M     "+"Kpoint mesh" + '\n')
-    genSCoutput.data.append("----------------------------------\n")
-    print ("---------------------------------")
-    print ("ScalingLMN " "    V_M     " "Kpoint mesh")
-    print ("---------------------------------")
-    for j in range(len(lmn_list)):
-        dirname=str(lmn_list[j][0])+"x"+str(lmn_list[j][1])+"x"+str(lmn_list[j][2])
-        #os.mkdir(dirname)
-        mkdir_p(dirname)
-        #shutil.copy("INCAR", dirname)
-        #shutil.copy("POTCAR",dirname)
-        os.chdir(dirname)
-        dummystruct=primordial.copy()
-        dummystruct.make_supercell(lmn_list[j])
-        dummykpnt=mg.io.vaspio.Kpoints.automatic_density(dummystruct,1500)
-        mg.write_structure(dummystruct,'POSCAR')
-        dummykpnt.write_file('KPOINTS')
-        print (str(lmn_list[j]) +" "+ str(EneVsVm.CalcV_M(dummystruct)) +" "+ str(dummykpnt.kpts[0]))
-        genSCoutput.data.append((str(lmn_list[j]) +" "+ str(EneVsVm.CalcV_M(dummystruct)) +" "+ str(dummykpnt.kpts[0]))+"\n")
-        os.chdir(cwdir)
-    genSCoutput.to_file(os.path.join(cwdir,"supercell_list.txt"))
+        Vm_list_raw_copy=list(Vm_list_raw)
+        
+        VmClosest2binBounds=list2Bins(Vm_list_raw,numStructAsked-1)
+        
+        VmSelected=[]        
+        for kk in VmClosest2binBounds:
+            if kk!=None:
+                VmSelected.append(kk)                                              
+                Vm_list_raw_copy.remove(kk)
                 
-# def CalcNelect(defectCharge):  
-"""
-This is to calculate the NELECT parameter based ond defect charge and composition
-"""
+        while len(VmSelected) < numStructAsked:                       
+            numStructDeficit=numStructAsked-len(VmSelected)
 
-#Dirs = ["2x2x1","2x2x2","2x2x3","3x3x1","4x4x1","3x3x2"]
-#for i in range(len(Dirs)):
-#    PrLatMat=mg.io.vaspio.Poscar.from_file(Dirs[i]+"_VAl3+.vasp").structure.lattice
-#    print Dirs[i]
-#    print PrLatMat.a
-#    print PrLatMat.b
-#    print PrLatMat.c 
-#    print minVertDist(PrLatMat)      
+            if (numStructDeficit == 1):
+                Vm_list_raw_copy_sorted=sorted(Vm_list_raw_copy)
+                dummyVm=Vm_list_raw_copy_sorted[roof_mean(0,len(Vm_list_raw_copy_sorted))]
+                VmSelected.append(dummyVm)                                              
+                #Vm_list_raw_copy.remove(dummyVm)
+            else:
+                VmClosest2binBounds_loop=list2Bins(Vm_list_raw_copy,numStructDeficit-1)
+                for mm in VmClosest2binBounds_loop:
+                    if mm!=None:
+                        VmSelected.append(mm)
+                        Vm_list_raw_copy.remove(mm)
+                        
+        for Vm in sorted(VmSelected):
+            LMN_list.append(Vm_LMN_dict[Vm])
 
-#testlat=mg.core.lattice.Lattice([[1,0,0],
-#                                 [0.9,0.1,0],
-#                                 [0.8,0.05,0.1]])
-#print testlat.a #1.0
-#print testlat.b #0.905538513814
-#print testlat.c #0.80777472107
-#print minVertDist(testlat) #0.141421356237
+    print ("The following are selected supercells:")   
+    print ("ScalingLMN  " " V_M")
+    print ("-----------------")           
+    for j in range(len(LMN_list)):
+        dummystruct=primordial_struct.copy()
+        dummystruct.make_supercell(LMN_list[j])
+        print (str(LMN_list[j])+"   "+
+               str(round(EneVsVm.CalcV_M(dummystruct),2))) 
+    print ""
+    return LMN_list
 
+def gensc(LMN_list,perf_primordial_struct, 
+          def_primordial_struct,def_primordial_kpnt,
+          def_primordial_potcar,def_primordial_incar):
 
-##kk=mg.io.vaspio.Poscar.from_file("POSCAR_pmcell_SiC").structure
-#kk=mg.io.vaspio.Poscar.from_file("POSCAR").structure
-#ll=genLMN(kk,4,1000)
-##print ll
-#genSCs(kk,ll)
+    if ('MAGMOM' in def_primordial_incar):
+        print ("WARNING: found MAGMOM in the primordial cell INCAR" 
+                "please manually reset MAGMOM in the supercell INCAR "
+                "to match it to the number of atoms. Otherwise the job will crash.")
+    
+    defchg=defChg(def_primordial_struct,def_primordial_potcar,def_primordial_incar)
+    print("The defect charge is "+str(defchg)+" in the defected primordial cell.")
+    print""               
+       
+    cwdir = os.getcwd()
+    print("To run the following supercells:")
+    print ("ScalingLMN " "  V_M    " " Kpoint  " " Natoms")
+    print ("-------------------------------------") 
+    for j in range(len(LMN_list)):
 
+        dirname=str(LMN_list[j][0])+"x"+str(LMN_list[j][1])+"x"+str(LMN_list[j][2])
+        mkdir_p(dirname)
+        os.chdir(dirname)
+        
+        dummystruct=perf_primordial_struct.copy()
+        dummystruct.make_supercell(LMN_list[j])
+        ####The next section does embeding#######            
+
+        ####This above section does embeding#######
+        mg.write_structure(dummystruct,'POSCAR') 
+       
+        kppra=round((def_primordial_kpnt.kpts[0][0]*
+                def_primordial_kpnt.kpts[0][1]*
+                def_primordial_kpnt.kpts[0][2])/
+                (1/def_primordial_struct.composition.num_atoms))        
+        dummykpnt=mg.io.vaspio.Kpoints.automatic_density(dummystruct,kppra)
+        dummykpnt.style=def_primordial_kpnt.style
+        dummykpnt.write_file('KPOINTS')
+                
+        dummystruct_natoms_el=num_atoms_speciewise(dummystruct)
+        dummynelect=-defchg
+        for i in range(len(def_primordial_potcar)):
+            dummyZval=def_primordial_potcar[i].ZVAL
+            dummynelect=dummynelect+dummyZval*dummystruct_natoms_el[i]
+        def_primordial_incar.__setitem__('NELECT',dummynelect)
+        def_primordial_incar.write_file('INCAR')
+        
+        def_primordial_potcar.write_file('POTCAR')
+        print (str(LMN_list[j])+"   "+
+               str(round(EneVsVm.CalcV_M(dummystruct),2))+"   "+
+               str(dummykpnt.kpts[0])+"   "+
+               str(int(dummystruct.composition.num_atoms)))
+ 
+        os.chdir(cwdir)
+        
+if __name__ == "__main__":
+    saved = sys.stdout
+    fout = file('out.log', 'w')
+    sys.stdout = writer(sys.stdout, fout)
+    
+    perfDir = "primordial_perfect"
+    defDir = "primordial_defected"
+    
+
+    jj=mg.io.vaspio.Poscar.from_file(perfDir+"_CONTCAR").structure 
+
+    kk=mg.io.vaspio.Poscar.from_file(defDir+"_CONTCAR").structure
+    ll=mg.io.vaspio.Kpoints.from_file(defDir+"_KPOINTS")
+    mm=mg.io.vaspio.Potcar.from_file(defDir+'POTCAR')
+    nn=mg.io.vaspio.Incar.from_file(defDir+'INCAR')
+
+    LMN_list=genLMNs(jj,3,600,20)
+
+    gensc(LMN_list,jj,kk,ll,mm,nn)
+
+    sys.stdout = saved
+    fout.close()
