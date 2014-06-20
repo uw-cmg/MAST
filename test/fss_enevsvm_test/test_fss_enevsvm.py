057d402de8e5613de9a26e536a8232f7b5db593f
Zhewen Song <zsong39@wisc.edu>
2014-06-20 11:12:14 -0500

    fss unit tests added

---
+++ test/fss_enevsvm_test/test_fss_enevsvm.py
@@ -0,0 +1,34 @@
+import os
+import time
+import unittest
+from unittest import SkipTest
+import filecmp
+from filecmp import dircmp
+import MAST
+import shutil
+import pymatgen
+import numpy as np
+from MAST.utility.finite_size_scaling.EneVsVm import CalcV_M
+testname ="fss_enevsvm_test"
+#oldcontrol = os.getenv("MAST_CONTROL")
+#oldrecipe = os.getenv("MAST_RECIPE_PATH")
+#oldscratch = os.getenv("MAST_SCRATCH")
+#print "Old directories:"
+#print oldcontrol
+#print oldrecipe
+#print oldscratch
+testdir = os.path.join(os.getenv("MAST_INSTALL_PATH"),'test',testname)
+
+
+class TestEneVsVm(unittest.TestCase):
+    """Test EneVsVm
+    """
+    def setUp(self):
+        os.chdir(testdir)
+
+    def tearDown(self):
+        pass
+    def test_genLMNs(self):
+        struct = pymatgen.io.vaspio.Poscar.from_file("POSCAR").structure
+        V_M = round(5.185227,5)
+        self.assertEqual(V_M,round(CalcV_M(struct),5)) 
---
+++ test/fss_gensc_test/test_fss_gensc.py
@@ -0,0 +1,36 @@
+import os
+import time
+import unittest
+from unittest import SkipTest
+import filecmp
+from filecmp import dircmp
+import MAST
+import shutil
+import pymatgen
+import numpy as np
+from MAST.utility.finite_size_scaling.GenSC import genLMNs
+from MAST.utility.finite_size_scaling.GenSC import gensc
+testname ="fss_gensc_test"
+#oldcontrol = os.getenv("MAST_CONTROL")
+#oldrecipe = os.getenv("MAST_RECIPE_PATH")
+#oldscratch = os.getenv("MAST_SCRATCH")
+#print "Old directories:"
+#print oldcontrol
+#print oldrecipe
+#print oldscratch
+testdir = os.path.join(os.getenv("MAST_INSTALL_PATH"),'test',testname)
+
+
+class TestGenSC(unittest.TestCase):
+    """Test GenSC
+    """
+    def setUp(self):
+        os.chdir(testdir)
+
+    def tearDown(self):
+        pass
+    def test_genLMNs(self):
+        primordial_struct = pymatgen.io.vaspio.Poscar.from_file("POSCAR_perfect").structure
+        LMNlist = [[1, 1, 2], [2, 2, 2], [1, 2, 2], [1, 1, 1]]
+        self.assertEqual(LMNlist,genLMNs(primordial_struct,minDefDist=5,maxNumAtoms=600,numStructAsked=5))
+        
