2a0a44943adec8f85671020c14842daff41a3bd7
Doyel <saswati@korczak.msae.wisc.edu>
2014-07-02 14:02:02 -0500

    Corrected the tabs in the file

--- MAST/controllers/mastmon.py
+++ MAST/controllers/mastmon.py
@@ -70,8 +70,8 @@ class MASTmon(object):
         os.chdir(fulldir) #need to change directories in order to submit jobs?
         myipparser = InputParser(inputfile=os.path.join(fulldir, 'input.inp'))
         myinputoptions = myipparser.parse()
-		input_options_keys = myinputoptions.get_sections()
-		key = 'personal_recipe'
+	input_options_keys = myinputoptions.get_sections()
+	key = 'personal_recipe'
         if key in input_options_keys:
             self.logger.info("Key - personal recipe was found")
 		personal_recipe_contents = myinputoptions.get_item('personal_recipe', 'personal_recipe_file')
