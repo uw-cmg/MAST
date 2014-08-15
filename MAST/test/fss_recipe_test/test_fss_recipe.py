024b7be14a65925f33d0c316e2d0d978c45b3a6a
cmgtam <mayeshiba@wisc.edu>
2014-08-15 12:04:37 -0500

    Update FSS recipe test to comply with new recipe-reading format

--- MAST/test/fss_recipe_test/test_fss_recipe.py
+++ MAST/test/fss_recipe_test/test_fss_recipe.py
@@ -7,7 +7,8 @@ import time
 import MAST
 import pymatgen
 from MAST.utility import dirutil
-
+from MAST.recipe import recipeutility
+from MAST.utility import MASTFile
 testname="fss_recipe_test"
 testdir = os.path.join(dirutil.get_mast_install_path(),'test',testname)
 
@@ -20,7 +21,10 @@ class TestRecipeUtility(unittest.TestCase):
         pass
 
     def test_read_recipe(self):
-        [htu, ptc,htr, rname]=MAST.recipe.recipeutility.read_recipe(os.path.join(testdir,'fss_recipe.txt'))
+        fss_recipe = MASTFile(os.path.join(testdir,'fss_recipe.txt'))
+        fss_recipe_lines = fss_recipe.data
+        fss_recipe_lines.pop(0) #remove recipe name
+        [htu, ptc,htr]=recipeutility.read_recipe(fss_recipe_lines)
         self.assertEqual(htu['defect_2x2x3_int1_opt']['neb_2x2x3_int1-int2_opt'],'relax_to_neb')
         self.assertItemsEqual(ptc['neb_1x2x4_int1-int2_opt'],['defect_1x2x4_int1_opt','defect_1x2x4_int2_opt'])
         self.assertEqual(htr['phonon_1x1x2_int1-int2_w0'],'phonon')
