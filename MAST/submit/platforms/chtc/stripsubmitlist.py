9df9c53db1d594de0f8fafad0c75d4571d4b8ab0
cmgtam <mayeshiba@wisc.edu>
2014-08-28 14:03:34 -0500

    Fix TEMP prepend for submitlist on CHTC

---
+++ MAST/submit/platforms/chtc/stripsubmitlist.py
@@ -0,0 +1,36 @@
+#!/bin/env python
+############
+# TTM 2014-08-28 nshd_mast
+############
+import os
+import sys
+#import optparse
+#from MAST.utility.mastfile import MASTFile
+#from MAST.utility import dirutil
+#from MAST.recipe.recipeplan import RecipePlan
+
+def main():
+    """Strip $TEMP directory out of the submission list."""
+    subname="%s/submitlist" % os.getenv("MAST_CONTROL")
+    sublist=open(subname,"rb")
+    mylines = sublist.readlines()
+    sublist.close()
+    newlines=list()
+    newline=""
+    for myline in mylines:
+        newline=""
+        if "/var/lib/" in myline:
+            mysplit = myline.split("/")
+            ingname = mysplit[-1]
+            recipename = mysplit[-2]
+            newline = "MAST/SCRATCH/%s/%s\n" % (recipename, ingname)
+            newlines.append(newline)
+        else:
+            newlines.append(myline)
+    sublist=open(subname,"wb")
+    sublist.writelines(newlines)
+    sublist.close()
+    return
+
+if __name__ == "__main__":
+    main()
