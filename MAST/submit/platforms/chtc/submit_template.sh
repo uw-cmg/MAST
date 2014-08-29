a568cd267263b4184b45f1fad671f5650accafdb
cmgtam <mayeshiba@wisc.edu>
2014-08-29 15:19:33 -0500

    Modify for large-file support for submit-3 chtc

--- MAST/submit/platforms/chtc/submit_template.sh
+++ MAST/submit/platforms/chtc/submit_template.sh
@@ -2,20 +2,20 @@
 universe = vanilla
 executable = //home/tmayeshi/bin/wrapper_exec
 arguments = ?mast_exec? ?mast_ppn?
-transfer_input_files = ./,?mast_exec?,//home/tmayeshi/openmpi
+transfer_input_files = ./
 should_transfer_files = yes
 when_to_transfer_output = on_exit
 request_cpus = ?mast_ppn?
 request_memory = 1000
 request_disk = 1000
 
-#Requirements = CAN_RUN_WHOLE_MACHINE
+Requirements = CAN_RUN_WHOLE_MACHINE
 
 log = log
 output = output
 error  = error
 getenv = true
-#+RequiresWholeMachine=true
++RequiresWholeMachine=true
 #+AccountingGroup = MSE_Morgan
 +WantFlocking = True
 +WantGlideIn = True
