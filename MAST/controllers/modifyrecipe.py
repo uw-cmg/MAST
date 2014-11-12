75ba41ca6680927bd961ba86eb43c3ee17f344eb
cmgtam <mayeshiba@wisc.edu>
2014-11-11 20:42:19 -0600

    Modify recipe

--- MAST/controllers/modifyrecipe.py
+++ MAST/controllers/modifyrecipe.py
@@ -2,7 +2,7 @@
 # This code is part of the MAterials Simulation Toolkit (MAST)
 # 
 # Maintainer: Tam Mayeshiba
-# Last updated: 2014-04-25
+# Last updated: 2014-11-11
 ##############################################################
 import os
 import time
@@ -44,14 +44,8 @@ class ModifyRecipe(MASTObj):
                 options parsed from the input file
             self.recipe_plan <RecipePlan object>: Stores the
                 recipe plan parsed from self.input_options
-            #self.origin_dir <str>: Original directory for the
-                                   mast -i *.inp command
-            #self.timestamp <str>: Timestamp of the mast -i *.inp
-                                  command
-            #self.asctime <str>: ASCII timestamp
-            #self.working_directory <str>: Working directory 
-                created for new recipe from mast -i *.inp command
-            #self.sysname <str>: System name (elements)
+            self.timestamp <str>: ASCII timestamp in YYYYMMDD'T'HHmmss format
+            self.asctime <str>: ASCII timestamp
             self.logger <logging logger>
     """
     def __init__(self, **kwargs):
@@ -60,7 +54,6 @@ class ModifyRecipe(MASTObj):
         self.input_options = None
         self.timestamp=""
         self.asctime=""
-        self.sysname=""
         self.recipe_plan = None
         self.logger = loggerutils.initialize_short_logger(os.path.join(get_mast_control_path(),"mast.log"))
 
@@ -89,8 +82,7 @@ class ModifyRecipe(MASTObj):
         """
         self.set_input_options()
         self.set_class_attributes()
-        self.make_working_directory()
-        self.create_recipe_metadata()
+        self.update_recipe_metadata()
         self.copy_input_file()
         self.parse_recipe_template()
         self.create_recipe_plan()
@@ -103,38 +95,19 @@ class ModifyRecipe(MASTObj):
         setup_obj = RecipeSetup(recipeFile=personal_recipe_contents, 
                 inputOptions=self.input_options,
                 structure=self.input_options.get_item('structure','structure'), 
-                workingDirectory=self.working_directory
+                workingDirectory=self.recdir
                 )
         self.recipe_plan = setup_obj.start()
         self.recipe_plan.print_status()
         return
 
-    def make_working_directory(self):
-        """Initialize the directory for writing the 
-            recipe and ingredient folders.
-        """
-        try:
-            os.mkdir(self.working_directory)
-        except:
-            MASTError(self.__class__.__name__, "Cannot create working directory %s !!!" % self.working_directory)
 
-    def copy_input_file(self):
-        """Copy the input file to input.inp
-            If the original file had loops, this is a single,
-            non-looped input file.
-        """
-        shutil.copy(self.keywords['inputfile'], 
-            os.path.join(self.working_directory,'input.inp'))
 
-    def create_recipe_metadata(self):
-        """Create the recipe metadata file.
+    def update_recipe_metadata(self):
+        """Update the recipe metadata file.
         """
-        topmeta = Metadata(metafile="%s/metadata.txt" % self.working_directory)
-        topmeta.write_data('directory_created', self.asctime)
-        topmeta.write_data('system_name', self.sysname)
-        topmeta.write_data('origin_dir', self.origin_dir)
-        topmeta.write_data('working_directory', self.working_directory)
-        topmeta.write_data('timestamp', self.timestamp)
+        topmeta = Metadata(metafile="%s/metadata.txt" % self.recdir)
+        topmeta.write_data('recipe_updated', self.asctime)
         return
 
     def create_archive_files(self):
@@ -145,28 +118,20 @@ class ModifyRecipe(MASTObj):
         """
         inputsave = MASTFile()
         inputsave.data = repr(self.input_options)
-        inputsave.to_file(os.path.join(self.working_directory, 'archive_input_options.txt'))
+        inputsave.to_file(os.path.join(self.working_directory, 'archive_input_options_%s.txt' % self.timestamp))
 
         recipesave = MASTFile()
         recipesave.data = repr(self.recipe_plan)
-        recipesave.to_file(os.path.join(self.working_directory, 'archive_recipe_plan.txt'))
+        recipesave.to_file(os.path.join(self.working_directory, 'archive_recipe_plan_%s.txt' % self.timestamp))
 
-        #pickle_plan = os.path.join(self.working_directory, 'archive_recipe_plan.pickle')
-        #pm = PickleManager(pickle_plan)
-        #pm.save_variable(self.recipe_plan)
-        
-        #pickle_options = os.path.join(self.working_directory, 'archive_input_options.pickle')
-        #pm = PickleManager(pickle_options)
-        #pm.save_variable(self.input_options)
-
-        #create the *.py input script
-        #ipc_obj = InputPythonCreator(input_options=self.input_options)
-        #ipc_filename = ipc_obj.write_script(self.working_directory, 'archive_input_options.py')
         return
 
     def parse_recipe_template(self):
         """Parses the recipe template file."""
-
+        ipkeys = self.input_options.get_sections()
+        if 'personal_recipe' in ipkeys:
+            raise MASTError(self.__class__.__name__, "Personal recipe section still exists in input.inp. Remove this section and try the command again.")
+            return
         recipe_file_contents = self.input_options.get_item('recipe', 'recipe_file')
 
         parser_obj = RecipeTemplateParser(templateFile=recipe_file_contents, 
@@ -187,72 +152,4 @@ class ModifyRecipe(MASTObj):
         time.sleep(1)
         self.timestamp = time.strftime('%Y%m%dT%H%M%S')
         self.asctime = time.asctime()
-        self.set_sysname()
-        self.set_origin_dir()
-        self.set_working_directory()
-    
-    def set_origin_dir(self):
-        """Set the origin directory (input file directory)
-        """
-        self.origin_dir = os.path.dirname(self.keywords['inputfile'])
-        if self.origin_dir == "":
-            self.origin_dir = os.getcwd()
-        return
-
-    def set_sysname(self):
-        """Set system name."""
-        element_str = self.get_element_map_string()
-        if element_str == None:
-            element_str = self.get_element_string()
-        system_name = self.input_options.get_item("mast", "system_name", "sys")
-        self.sysname = system_name + '_' + element_str
-        return
-
-    def set_working_directory(self):
-        """Get the system name and working directory.
-        """
-        #recipename = os.path.basename(self.input_options.get_item('recipe','recipe_file')).split('.')[0]
-        #dir_name = "%s_%s_%s" % (self.sysname, recipename, self.timestamp)
-        dir_name = "%s_%s" % (self.sysname, self.timestamp)
-        dir_path = str(os.path.join(get_mast_scratch_path(), dir_name))
-        self.working_directory = dir_path
-        return
-
-    def get_element_string(self):
-        """Get the element string from the structure.
-        """
-        mystruc = self.input_options.get_item('structure','structure')
-        elemset = set(mystruc.species)
-        elemstr=""
-        elemok=1
-        while elemok == 1:
-            try:
-                elempop = elemset.pop()
-                elemstr = elemstr + elempop.symbol
-            except KeyError:
-                elemok = 0
-        return elemstr
-    def get_element_map_string(self):
-        """Get the element string from the elementmap section.
-            Returns:
-                elemstr <str>: element string from elementmap
-                                section of the input file,
-                                in order of X1, X2, etc.
-                If no elementmap section exists in the input file
-                (which, when put in dictionary form, has the
-                subsection name element_map), then the method
-                returns None
-        """
-        elemmap = self.input_options.get_item('structure','element_map')
-        if elemmap == None:
-            return None
-        elkeys = elemmap.keys()
-        elkeys.sort()
-        elstr=""
-        for elkey in elkeys:
-            elstr = elstr + elemmap[elkey]
-        if elstr == "":
-            return None
-        return elstr
-
 
