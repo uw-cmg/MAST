c17f3cc964b1b6353b91612afbabfe7d80b98def
cmgtam <mayeshiba@wisc.edu>
2015-07-30 13:58:05 -0500

    Add new files for MAST installation directly in gluster; modify comparescratch.py so that change_status.txt files are recognized

--- MAST/submit/platforms/chtc/comparescratch.py
+++ MAST/submit/platforms/chtc/comparescratch.py
@@ -17,81 +17,115 @@ def main():
     (myopt, myarg) = parser.parse_args()
     if myopt.mode == "save":
         save_old_status_files()
+        save_old_change_status_files()
     elif myopt.mode == "compare":
-        compare_old_and_new_status_files()
+        merge_dictionaries_and_write_recipechangefile()
     elif myopt.mode == "zip":
         zip_only_changed()
     else:
         print "No option selected. Exiting."
     return
 
-
-def save_recipe_files(listfilename="",whichfilename=""):
-    """Save off old files from recipe in $MAST_SCRATCH
-       into $MAST_CONTROL
-       Args:
-        listfilename <str>: File name for the list of files to be saved
-        whichfilename <str>: Short file name to be saved (identical file
-                                for all recipes)
-    """
-    if listfilename == "":
-        raise NameError("No list file name given.")
-    if whichfilename == "":
-        raise NameError("No file name given for which files to save.")
-    mastcontrol=dirutil.get_mast_control_path()
-    if not os.path.exists(os.path.join(mastcontrol,listfilename)):
-        os.mkdir(os.path.join(mastcontrol,listfilename))
-    mastscratch=dirutil.get_mast_scratch_path()
-    recipedirs=dirutil.immediate_subdirs(mastscratch)
-    for recipedir in recipedirs:
-        statusfile = MASTFile(os.path.join(mastscratch,recipedir,whichfilename))
-        trydir = os.path.join(mastcontrol,listfilename,recipedir)
-        if not os.path.exists(trydir):
-            os.mkdir(trydir)
-        statusfile.to_file("%s/%s" % (trydir, whichfilename))
-    return True
-
-def save_ingredient_files(listfilename="",whichfilename=""):
-    """Save off old files from ingredients in $MAST_SCRATCH
-       into $MAST_CONTROL
-       Args:
-        listfilename <str>: File name for the list of files to be saved
-        whichfilename <str>: Short file name to be saved (identical file
-                                for all recipes)
-    """
-    if listfilename == "":
-        raise NameError("No list file name given.")
-    if whichfilename == "":
-        raise NameError("No file name given for which files to save.")
-    mastcontrol=dirutil.get_mast_control_path()
-    if not os.path.exists(os.path.join(mastcontrol,listfilename)):
-        os.mkdir(os.path.join(mastcontrol,listfilename))
-    mastscratch=dirutil.get_mast_scratch_path()
-    recipedirs=dirutil.immediate_subdirs(mastscratch)
-    for recipedir in recipedirs:
-        recipefulldir=os.path.join(mastscratch,recipedir)
-        ingreddirs=dirutil.immediate_subdirs(recipefulldir)
-        for ingreddir in ingreddirs:
-            statfilepath = os.path.join(recipefulldir, ingreddir, whichfilename)
-            if os.path.exists(statfilepath):
-                statusfile = MASTFile(statfilepath)
-                trydir = os.path.join(mastcontrol,listfilename,recipedir,ingreddir)
-                if not os.path.exists(trydir):
-                    os.mkdir(trydir)
-                statusfile.to_file("%s/%s" % (trydir, whichfilename))
-    return True
-
 def save_old_status_files():
     """Save off old status files from $MAST_SCRATCH
        into $MAST_CONTROL
     """
-    return save_recipe_files("statusfiles","status.txt")
+    mastcontrol=dirutil.get_mast_control_path()
+    if not os.path.exists(os.path.join(mastcontrol,"statusfiles")):
+        os.mkdir(os.path.join(mastcontrol,"statusfiles"))
+    mastscratch=dirutil.get_mast_scratch_path()
+    recipedirs=dirutil.immediate_subdirs(mastscratch)
+    for recipedir in recipedirs:
+        statusfile = MASTFile(os.path.join(mastscratch,recipedir,"status.txt"))
+        trydir = os.path.join(mastcontrol,"statusfiles",recipedir)
+        if not os.path.exists(trydir):
+            os.mkdir(trydir)
+        statusfile.to_file("%s/status.txt" % trydir)
+    return True
 
 def save_old_change_status_files():
-    """Save off old change_status.txt files from ingredient directories
-        into $MAST_CONTROL
+    """Save off old status files from $MAST_SCRATCH
+       into $MAST_CONTROL
     """
-    return save_ingredient_files("changestatusfiles","change_status.txt")
+    mastcontrol=dirutil.get_mast_control_path()
+    if not os.path.exists(os.path.join(mastcontrol,"changestatusfiles")):
+        os.mkdir(os.path.join(mastcontrol,"changestatusfiles"))
+    mastscratch=dirutil.get_mast_scratch_path()
+    recipedirs=dirutil.immediate_subdirs(mastscratch)
+    for recipedir in recipedirs:
+        ingreddirs=dirutil.immediate_subdirs(os.path.join(mastscratch,recipedir))
+        for ingreddir in ingreddirs:
+            csfile = os.path.join(mastscratch,recipedir,ingreddir,"change_status.txt")
+            if os.path.isfile(csfile):
+                changestatusfile = MASTFile(csfile)
+                trydir = os.path.join(mastcontrol,"changestatusfiles",recipedir)
+                if not os.path.exists(trydir):
+                    os.mkdir(trydir)
+                trydir2 = os.path.join(trydir, ingreddir)
+                if not os.path.exists(trydir2):
+                    os.mkdir(trydir2)
+                    changestatusfile.to_file("%s/change_status.txt" % trydir2)
+    return True
+
+def compare_old_and_new_change_status_files():
+    """Compare old and new status files from $MAST_CONTROL
+        and $MAST_SCRATCH.
+        Write an indication whether the status files have changed,
+        not changed. Use "archived" if a recipe has been archived or is
+        missing from SCRATCH for some other reason.
+    """
+    rdict=dict()
+    mastcontrol=dirutil.get_mast_control_path()
+    mastscratch=dirutil.get_mast_scratch_path()
+    recipedirs=dirutil.immediate_subdirs(os.path.join(mastcontrol,"changestatusfiles"))
+    for recipedir in recipedirs:
+        mystatus="unknown"
+        rdict[recipedir]=dict()
+        changelist=list()
+        if not os.path.exists(os.path.join(mastscratch,recipedir)):
+            mystatus="archived"
+        else:
+            ingreddirs = dirutil.immediate_subdirs(os.path.join(mastcontrol,"changestatusfiles",recipedir))
+            for ingreddir in ingreddirs:
+                scratchstatusfile = MASTFile(os.path.join(mastscratch,recipedir,ingreddir,"change_status.txt"))
+                controlstatusfile = MASTFile(os.path.join(mastcontrol,"changestatusfiles",recipedir,ingreddir,"change_status.txt"))
+                if scratchstatusfile.data == controlstatusfile.data:
+                    mystatus="unchanged"
+                else:
+                    mystatus="changed"
+                    rdict[recipedir][ingreddir]="send"
+            rdict[recipedir]["MAIN"]=mystatus
+    return rdict
+
+def get_new_change_status_files():
+    """Compare old and new status files from $MAST_CONTROL
+        and $MAST_SCRATCH.
+        Write an indication whether the status files have changed,
+        not changed. Use "archived" if a recipe has been archived or is
+        missing from SCRATCH for some other reason.
+    """
+    rdict=dict()
+    mastcontrol=dirutil.get_mast_control_path()
+    mastscratch=dirutil.get_mast_scratch_path()
+    olddirs=list()
+    recipedirs=dirutil.immediate_subdirs(os.path.join(mastcontrol,"changestatusfiles"))
+    for recipedir in recipedirs:
+        ingreddirs = dirutil.immediate_subdirs(os.path.join(mastcontrol,"changestatusfiles",recipedir))
+        for ingreddir in ingreddirs:
+            olddirs.append("%s/%s" % (recipedir, ingreddir))
+    srecdirs = dirutil.immediate_subdirs(mastscratch)
+    for srecdir in srecdirs:
+        singreddirs = dirutil.immediate_subdirs(os.path.join(mastscratch,srecdir))
+        for singreddir in singreddirs:
+            csfile = os.path.join(mastscratch, srecdir, singreddir, "change_status.txt")
+            if os.path.isfile(csfile):
+                if not "%s/%s" % (srecdir, singreddir) in olddirs:
+                    if not srecdir in rdict.keys():
+                        rdict[srecdir]=dict()
+                    rdict[srecdir]["MAIN"]="changed"
+                    rdict[srecdir][singreddir]="send"
+    return rdict
+
 
 def compare_old_and_new_status_files():
     """Compare old and new status files from $MAST_CONTROL
@@ -110,7 +144,7 @@ def compare_old_and_new_status_files():
         changelist=list()
         if not os.path.exists(os.path.join(mastscratch,recipedir)):
             mystatus="archived"
-	else:
+        else:
             scratchstatusfile = MASTFile(os.path.join(mastscratch,recipedir,"status.txt"))
             controlstatusfile = MASTFile(os.path.join(mastcontrol,"statusfiles",recipedir,"status.txt"))
             if scratchstatusfile.data == controlstatusfile.data:
@@ -135,19 +169,42 @@ def compare_old_and_new_status_files():
                              rdict[recipedir][ingred]="send"
                     myidx = myidx + 1
         rdict[recipedir]["MAIN"]=mystatus
-    recipedirs=dirutil.immedate_subdirs(os.path.join(mastcontrol,"changestatusfiles"))
-    for recipedir in recipedirs:
-        ingreddirs=dirutil.immediate_subdirs(os.path.join(mastcontrol,"changestatusfiles",recipedir))
-        for ingreddir in ingreddirs:
-            oldchangestatus=MASTFile(os.path.join(mastcontrol,"changestatusfiles",recipedir,ingreddir,"change_status.txt"))
-            newchangestatus=MASTFile(os.path.join(mastscratch,recipedir,ingreddir,"change_status.txt"))
-            if not (oldchangestatus.data == newchangestatus.data):
-                rdict[recipedir]["MAIN"]="changed"
-                rdict[recipedir][ingreddir]="send"
+    return rdict
+
+def merge_dictionaries_and_write_recipechangefile():
+    rdict_status = compare_old_and_new_status_files() 
+    rdict_change_status = compare_old_and_new_change_status_files()
+    rdict_new_change_status = get_new_change_status_files()
     rchangefile = MASTFile()
+    rdict=dict()
+    for recdir in rdict_status.keys():
+        if not recdir in rdict.keys():
+            rdict[recdir]=dict()
+        for ingreddir in rdict_status[recdir].keys():
+            if not ingreddir in rdict[recdir].keys():
+                rdict[recdir][ingreddir] = rdict_status[recdir][ingreddir]
+    for recdir in rdict_change_status.keys():
+        if not recdir in rdict.keys():
+            rdict[recdir]=dict()
+        for ingreddir in rdict_change_status[recdir].keys():
+            if not ingreddir in rdict[recdir].keys():
+                rdict[recdir][ingreddir] = rdict_change_status[recdir][ingreddir]
+            else:
+                if rdict_change_status[recdir][ingreddir] == "send":
+                    rdict[recdir][ingreddir] = "send" #update to send
+    for recdir in rdict_new_change_status.keys():
+        if not recdir in rdict.keys():
+            rdict[recdir]=dict()
+        for ingreddir in rdict_new_change_status[recdir].keys():
+            if not ingreddir in rdict[recdir].keys():
+                rdict[recdir][ingreddir] = rdict_new_change_status[recdir][ingreddir]
+            else:
+                if rdict_new_change_status[recdir][ingreddir] == "send":
+                    rdict[recdir][ingreddir] = "send" #update to send
     for rdir in rdict.keys():
         for key, value in rdict[rdir].iteritems():
             rchangefile.data.append("%s:%s:%s\n" % (rdir,key,value))
+    mastcontrol=dirutil.get_mast_control_path()
     rchangefile.to_file(os.path.join(mastcontrol,"recipechangefile.txt"))
     return True
 
