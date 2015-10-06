2eecc05b17d2d90b2e9d013c384549774e7a5680
Tam Mayeshiba <mayeshiba@submit-3.chtc.wisc.edu>
2015-10-06 17:31:37 -0500

    Reduce number of ppn for testing, and set MAST directories inside scripts

--- MAST/submit/platforms/chtc_dagman/mast_check_is_complete.sh
+++ MAST/submit/platforms/chtc_dagman/mast_check_is_complete.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 date >> POST_script_output
+export MAST_SCRATCH="//home/mayeshiba/testing_MAST/SCRATCH"
+export MAST_ARCHIVE="//home/mayeshiba/testing_MAST/ARCHIVE"
+export MAST_CONTROL="//home/mayeshiba/testing_MAST/CONTROL"
 export MAST_PLATFORM="chtc_dagman"
 python //home/mayeshiba/DAG_for_MAST/mast_check_is_complete_python.py $MAST_SCRATCH"/"$1 $2 1>> POST_script_output 2>> POST_script_output
--- MAST/submit/platforms/chtc_dagman/mast_do_setup.sh
+++ MAST/submit/platforms/chtc_dagman/mast_do_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 date >> PRE_script_output
+export MAST_SCRATCH="//home/mayeshiba/testing_MAST/SCRATCH"
+export MAST_ARCHIVE="//home/mayeshiba/testing_MAST/ARCHIVE"
+export MAST_CONTROL="//home/mayeshiba/testing_MAST/CONTROL"
 export MAST_PLATFORM="chtc_dagman"
 python //home/mayeshiba/DAG_for_MAST/mast_do_setup_python.py $MAST_SCRATCH"/"$1 $2 1>> PRE_script_output 2>> PRE_script_output
