604f87161454646f9fe07e727495b808acdeaf6d
Tam Mayeshiba <mayeshiba@submit-3.chtc.wisc.edu>
2015-10-07 14:35:01 -0500

    add neb submit script switching

--- MAST/submit/platforms/chtc_dagman/mast_do_setup.sh
+++ MAST/submit/platforms/chtc_dagman/mast_do_setup.sh
@@ -9,3 +9,11 @@ export MAST_ARCHIVE=$mast_head_dir"/ARCHIVE"
 export MAST_CONTROL=$mast_head_dir"/CONTROL"
 export MAST_PLATFORM="chtc_dagman"
 python $script_head_dir"/mast_do_setup_python.py" $MAST_SCRATCH"/"$1 $2 1>> $output_file 2>> $output_file
+
+echo $2
+if [[ $2 == *"neb_"* ]]
+then 
+    echo "NEB detected; switching submission script" >> $output_file
+    cp $script_head_dir"/submit_neb.sh" $2"/submit.sh"
+else echo "not neb" >> $output_file
+fi
