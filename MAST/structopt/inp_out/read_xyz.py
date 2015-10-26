36685da45424aef9a88c7dc6f7ecce3c6a07eeeb
Tam Mayeshiba <mayeshiba@wisc.edu>
2015-10-26 14:25:47 -0500

    Put docstring back into function

--- MAST/structopt/inp_out/read_xyz.py
+++ MAST/structopt/inp_out/read_xyz.py
@@ -4,6 +4,21 @@ except ImportError:
     print "NOTE: ASE is not installed. To use Structopt read_xyz.py, ASE must be installed."
 
 def read_xyz(fileobj,n=-1,data=False):
+    """
+    Function to read multi-atom xyz file with data
+    Inputs:
+        fileobj = String containing file name or file object
+        n = Integer indicating number for structure from xyz file
+            to read. Default is last structure in file.
+            Or String=='All' which will output all structures in file
+        data = True/False boolean indicating whether or not to output 
+            any data strings read from xyz file
+    Outputs:
+        atmslist = ASE Atoms class object containing structure from file
+            or list of Atoms objects if n=='All'
+        datalist(Optional) = String of data read from xyz file or list 
+            of data strings read from xyz file
+    """
     if isinstance(fileobj, str):
         fileobj = open(fileobj,'r')
     lines = fileobj.readlines()
