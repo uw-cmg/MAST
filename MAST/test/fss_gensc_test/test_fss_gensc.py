a4ff927879d6fc95307d22915b5ec533a055fe32
cmgtam <mayeshiba@wisc.edu>
2016-01-15 14:13:27 -0600

    Fix test to compare list items instead of general equal

--- MAST/test/fss_gensc_test/test_fss_gensc.py
+++ MAST/test/fss_gensc_test/test_fss_gensc.py
@@ -33,5 +33,5 @@ class TestGenSC(unittest.TestCase):
     def test_genLMNs(self):
         primordial_struct = pymatgen.io.vaspio.Poscar.from_file("POSCAR_perfect").structure
         LMNlist = [[1, 1, 2], [2, 2, 2], [1, 2, 2], [1, 1, 1]]
-        self.assertEqual(LMNlist,genLMNs(primordial_struct,minDefDist=5,maxNumAtoms=600,numStructAsked=5))
+        self.assertItemsEqual(LMNlist,genLMNs(primordial_struct,minDefDist=5,maxNumAtoms=600,numStructAsked=5))
         
