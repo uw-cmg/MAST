03f030cae9cf6bd7699609330f074bfb91aa8375
cmgtam <mayeshiba@wisc.edu>
2016-07-20 13:49:22 -0500

    Comment out line. Close #439

--- MAST/submit/platforms/stampede/submit_template.sh
+++ MAST/submit/platforms/stampede/submit_template.sh
@@ -6,6 +6,6 @@
 #SBATCH -n ?mast_processors?
 #SBATCH -p ?mast_queue?
 #SBATCH --ntasks-per-node=?mast_ppn?
-module load python
+#module load python ###uncomment this line if not using anaconda python
 module load vasp
 ?mast_exec?
