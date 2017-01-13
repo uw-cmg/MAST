2882008e405bffd4784613691e3935ebfb9c80a8
cmgtam <mayeshiba@wisc.edu>
2017-01-13 15:23:52 -0600

    Add combination test files

---
+++ MAST/test/atom_index_test/example_input_files/neb_testboth_combination_AlMgCuPtAuAg_20170113T132336/neb_int1-int2_q=p0_pf_opt1/submit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -N neb_int1-int2_q=p0_pf_opt1
+#PBS -q morgan3
+#PBS -l nodes=1:ppn=32,pvmem=1000mb
+#PBS -l walltime=96:00:00
+##export all environment variables. 
+#PBS -V
+NN=`cat $PBS_NODEFILE | wc -l`
+echo "Processors received = "$NN
+echo "script running on host `hostname`"
+cd $PBS_O_WORKDIR
+echo "PBS_NODEFILE"
+cat $PBS_NODEFILE
+//opt/mpiexec/bin/mpiexec //share/apps/bin/vasp5.2_CNEB
