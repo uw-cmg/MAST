7041e6e475fce4e9fc8f07415090e27c2ecad83d
Tam Mayeshiba <mayeshiba@wisc.edu>
2017-03-17 14:44:17 -0500

    Add old workflows for reference

---
+++ MAST/test/workflow_test/savedoff/FiveFreq_AlMg_20140424T155453/phonon_1nn-solute_q=p0_movingsolute/submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#PBS -N phonon_1nn-solute_q=p0_movingsolute
+#PBS -l nodes=1:ppn=1,pvmem=1000mb
+#PBS -l walltime=4:00:00
+##export all environment variables. 
+#PBS -V
+NN=`cat $PBS_NODEFILE | wc -l`
+echo "Processors received = "$NN
+echo "script running on host `hostname`"
+cd $PBS_O_WORKDIR
+echo "PBS_NODEFILE"
+cat $PBS_NODEFILE
+//share/apps/vasp5.2_cNEB
---
+++ MAST/test/workflow_test/savedoff/FiveFreq_AlMg_20170317T111715/defect_solute_q=p0_opt2/submit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -N defect_solute_q=p0_opt2
+#PBS -q morgan2
+#PBS -l nodes=1:ppn=12,pvmem=1000mb
+#PBS -l walltime=96:00:00
+##export all environment variables. 
+#PBS -V
+NN=`cat $PBS_NODEFILE | wc -l`
+echo "Processors received = "$NN
+echo "script running on host `hostname`"
+cd $PBS_O_WORKDIR
+echo "PBS_NODEFILE"
+cat $PBS_NODEFILE
+//opt/mpiexec/bin/mpiexec //share/apps/bin/vasp5.2_CNEB
