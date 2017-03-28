161fb2f86d1df9aab340a2107fd1563ec119015a
Tam Mayeshiba <tmayeshi@login-knl1.stampede.tacc.utexas.edu>
2017-03-28 10:46:55 -0500

    Add stampede knl platform

---
+++ MAST/submit/platforms/stampede_knl/mastmon_submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH -t 1:00:00
+#SBATCH -J mastmon
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p development
+#SBATCH -o mastmon_submission_output
+#SBATCH -e mastmon_submission_errors
+module load vasp
+python runmast.py >> $MAST_CONTROL/mastoutput 2> $MAST_CONTROL/errormast
---
+++ MAST/submit/platforms/stampede_knl/submit_template.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH -t ?mast_walltime?
+#SBATCH -J ?mast_name?
+#SBATCH -N ?mast_nodes?
+#SBATCH -n ?mast_processors?
+#SBATCH -p ?mast_queue?
+#SBATCH --ntasks-per-node=?mast_ppn?
+module load vasp
+?mast_exec?
