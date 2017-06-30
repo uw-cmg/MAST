558633cb40f50bba4c9e86a6f23a18ca5ee26d32
cmgtam <mayeshiba@wisc.edu>
2017-06-30 11:11:50 -0500

    Update conf.py for read the docs try

--- MAST/doc/source/conf.py
+++ MAST/doc/source/conf.py
@@ -12,6 +12,11 @@
 # serve to show the default.
 
 import sys, os
+
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../../'))
 autodoc_mock_imports=['MAST',
                       'MAST.utility.dirutil',
@@ -21,11 +26,6 @@ autodoc_mock_imports=['MAST',
                         'pandas',
 ]
 from MAST.utility import dirutil
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('.'))
 #sys.path.insert(0, os.environ['MAST_INSTALL_PATH'])
 
 # -- General configuration -----------------------------------------------------
